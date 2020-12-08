f4db63314085fadd1594b48d3576f22e9887aee4
Mostafa <neo.kazemi168@gmail.com>
2020-12-08 10:01:40 +0330

    tinymce and mathlive now work together

--- src/components/TinymceEditor/TinymceEditor.vue
+++ src/components/TinymceEditor/TinymceEditor.vue
@@ -5,12 +5,19 @@
                 id="editor"
                 :init="initObj"
         />
-        <v-btn @click="addTextToEditor">click</v-btn>
+        <div dir="ltr">
+            <div id="mathfield" locale="fa">x=\frac{-b\pm \sqrt{b^2-4ac}}{2a}</div>
+            <div class="latexData" v-html="latexData"></div>
+        </div>
     </div>
 </template>
 
 <script>
     import Editor from '@tinymce/tinymce-vue'
+    import MathLive from 'mathlive';
+    import 'mathlive/dist/mathlive-fonts.css'
+    import 'mathlive/dist/mathlive-static.css'
+
     export default {
         name: "TinymceEditor",
         components: {
@@ -40,16 +47,34 @@
                         editor.ui.registry.addButton('addFormula', {
                             text: 'Add Formula',
                             icon: 'false',
-                            onAction: () => { this.that.addTextToEditor('hi') }
+                            onAction: () => { this.that.addTextToEditor('$' + this.that.latexData + '$') }
                         });
                     }
-                }
+                },
+                formula: '',
+                latexData: null
             }
         },
         methods: {
             addTextToEditor (text = '') {
                 tinymce.activeEditor.execCommand('mceInsertContent', false, text) // eslint-disable-line
             }
+        },
+        mounted() {
+            let that = this
+            const mf = MathLive.makeMathField(
+                document.getElementById('mathfield'),
+                {
+                    virtualKeyboardMode: 'manual',
+                    onContentDidChange: (mf) => {
+                        console.log(mf.getValue());
+                        that.latexData = mf.getValue()
+                    },
+                });
+            mf.$setConfig(
+                //{ macros: { ...mf.getConfig('macros'), smallfrac: '{}^{#1}\\!\\!/\\!{}_{#2}', }, }
+            );
+            that.latexData = mf.getValue()
         }
     }
 </script>
@@ -58,4 +83,18 @@
     .tox.tox-silver-sink.tox-tinymce-aux {
         display: none;
     }
+
+    .ML__virtual-keyboard-toggle {
+        color: gray !important;
+    }
+
+    #mathfield, .latexData {
+        font-size: 32px;
+        margin: 3em;
+        padding: 8px;
+        border-radius: 8px;
+        border: 1px solid rgba(0, 0, 0, .3);
+        box-shadow: 0 0 8px rgba(0, 0, 0, .2)
+    }
 </style>
+
