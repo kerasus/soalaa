c11ff3205a33d0e09948e0026526beaf55f5f159
neokazemi <neo.kazemi168@gmail.com>
2021-01-02 17:30:30 +0330

    some changes

--- src/components/OnlineQuiz/Quiz/ViewTypes/Konkoor.vue
+++ src/components/OnlineQuiz/Quiz/ViewTypes/Konkoor.vue
@@ -18,8 +18,147 @@
             </v-col>
             <v-col :md="7">
                 <v-row>
-                    <v-col>
+                    <v-col class="px-10 d-flex justify-space-between" dir="ltr">
+                        <div class="rounded-b-xl rounded-r-xl">
+                            <v-menu
+                                    bottom
+                                    :offset-y="true"
+                                    class="rounded-b-xl rounded-r-xl"
+                            >
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-btn
+                                            large
+                                            tile
+                                            v-bind="attrs"
+                                            v-on="on"
+                                            elevation="0"
+                                    >
+                                        <v-icon class="mr-2" :size="30" color="#666">mdi-account-circle</v-icon>
+                                        سید مصطفی
+                                        کاظمی
+                                    </v-btn>
+                                </template>
+                                <v-card
+                                        max-width="375"
+                                        class="mx-auto"
+                                        rounded="b-xl r-xl"
+                                >
+                                    <v-img
+                                            src="https://cdn.vuetifyjs.com/images/lists/ali.png"
+                                            height="300px"
+                                            dark
+                                    >
+                                        <v-row class="fill-height">
+                                            <v-card-title>
+                                                <v-btn
+                                                        dark
+                                                        icon
+                                                        :to="{ name: 'dashboard'}"
+                                                >
+                                                    <v-icon>mdi-chevron-left</v-icon>
+                                                </v-btn>
 
+                                                <v-spacer></v-spacer>
+
+                                                <v-btn
+                                                        dark
+                                                        icon
+                                                        class="mr-4"
+                                                >
+                                                    <v-icon>mdi-pencil</v-icon>
+                                                </v-btn>
+
+                                                <v-btn
+                                                        dark
+                                                        icon
+                                                >
+                                                    <v-icon>mdi-dots-vertical</v-icon>
+                                                </v-btn>
+                                            </v-card-title>
+
+                                            <v-spacer></v-spacer>
+
+                                            <v-card-title class="white--text pl-12 pt-12">
+                                                <div class="display-1 pl-12 pt-12">
+                                                    Ali Conners
+                                                </div>
+                                            </v-card-title>
+                                        </v-row>
+                                    </v-img>
+
+                                    <v-list two-line>
+                                        <v-list-item>
+                                            <v-list-item-icon>
+                                                <v-icon color="indigo">
+                                                    mdi-phone
+                                                </v-icon>
+                                            </v-list-item-icon>
+
+                                            <v-list-item-content>
+                                                <v-list-item-title>(650) 555-1234</v-list-item-title>
+                                                <v-list-item-subtitle>Mobile</v-list-item-subtitle>
+                                            </v-list-item-content>
+
+                                            <v-list-item-icon>
+                                                <v-icon>mdi-message-text</v-icon>
+                                            </v-list-item-icon>
+                                        </v-list-item>
+
+                                        <v-list-item>
+                                            <v-list-item-action></v-list-item-action>
+
+                                            <v-list-item-content>
+                                                <v-list-item-title>(323) 555-6789</v-list-item-title>
+                                                <v-list-item-subtitle>Work</v-list-item-subtitle>
+                                            </v-list-item-content>
+
+                                            <v-list-item-icon>
+                                                <v-icon>mdi-message-text</v-icon>
+                                            </v-list-item-icon>
+                                        </v-list-item>
+
+                                        <v-divider inset></v-divider>
+
+                                        <v-list-item>
+                                            <v-list-item-icon>
+                                                <v-icon color="indigo">
+                                                    mdi-email
+                                                </v-icon>
+                                            </v-list-item-icon>
+
+                                            <v-list-item-content>
+                                                <v-list-item-title>aliconnors@example.com</v-list-item-title>
+                                                <v-list-item-subtitle>Personal</v-list-item-subtitle>
+                                            </v-list-item-content>
+                                        </v-list-item>
+
+                                        <v-list-item>
+                                            <v-list-item-action></v-list-item-action>
+
+                                            <v-list-item-content>
+                                                <v-list-item-title>ali_connors@example.com</v-list-item-title>
+                                                <v-list-item-subtitle>Work</v-list-item-subtitle>
+                                            </v-list-item-content>
+                                        </v-list-item>
+
+                                        <v-divider inset></v-divider>
+
+                                        <v-list-item>
+                                            <v-list-item-icon>
+                                                <v-icon color="indigo">
+                                                    mdi-map-marker
+                                                </v-icon>
+                                            </v-list-item-icon>
+
+                                            <v-list-item-content>
+                                                <v-list-item-title>1400 Main Street</v-list-item-title>
+                                                <v-list-item-subtitle>Orlando, FL 79938</v-list-item-subtitle>
+                                            </v-list-item-content>
+                                        </v-list-item>
+                                    </v-list>
+                                </v-card>
+                            </v-menu>
+                        </div>
                     </v-col>
                 </v-row>
                 <v-row>
@@ -190,6 +329,8 @@ export default {
 .questions {
     background: #fff;
     overflow-y: auto;
+    position: relative;
+    padding-right: 25px;
 }
 
 .question {
@@ -253,5 +394,9 @@ export default {
 <style>
     .v-application p {
         margin-bottom: 4px;
+    }
+
+    html {
+        overflow-y: auto;
     }
 </style>
