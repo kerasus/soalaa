60bd2feacfbb8b19f1a84d8301132b58477bb234
Heliamh <helia.mhmdi@gmail.com>
2021-01-09 18:39:18 +0330

    exam model

---
+++ src/components/OnlineQuiz/Quiz/exams.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-row>
+
+        <v-col cols="4">
+            <exam-list/>
+        </v-col>
+        <v-col >
+            <exam-info/>
+        </v-col>
+    </v-row>
+
+</template>
+
+<script>
+    import ExamInfo from "@/components/OnlineQuiz/Quiz/examInfo";
+    import ExamList from "@/components/OnlineQuiz/Quiz/examList";
+
+    export default {
+        name: "exams",
+        components: {ExamList, ExamInfo}
+    }
+</script>
+
+<style scoped>
+
+</style>
