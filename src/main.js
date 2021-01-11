0735b1fcd3de2bf6ed9a1768b536f9ca99e87346
kerasus <aliesmaeeli20@gmail.com>
2021-01-11 14:13:31 +0330

    set token

--- src/main.js
+++ src/main.js
@@ -3,7 +3,17 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router/index';
 import store from "./store/store";
+import axios from 'axios'
 Vue.config.productionTip = false
+
+
+const accessToken = process.env.VUE_APP_ACCESS_TOKEN
+localStorage.setItem('access_token', accessToken)
+const token = localStorage.getItem('access_token')
+if (token) {
+  axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
+}
+
 
 new Vue({
   store,
