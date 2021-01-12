b6c7cb853758fd1745492dcfd3bfb486d5d0d169
kerasus <aliesmaeeli20@gmail.com>
2021-01-12 12:07:24 +0330

    timer

--- src/plugins/time.js
+++ src/plugins/time.js
@@ -1,5 +1,5 @@
 import moment from 'moment'
-import store from '@/store/store'
+// import store from '@/store/store'
 // import { Quiz } from '@/models/Quiz'
 
 let Time = function () {
@@ -8,16 +8,16 @@ let Time = function () {
     }
 
     function setStateOfExamCategories() {
-        const now = this.now()
-        store.getters.quiz.categories.list.forEach( (category, index, categories) => {
-            const prevCat = categories[index - 1]
-
-            if (prevCat && prevCat.end_at > now) {
-                category.is_active = false
-            } else category.is_active = category.end_at > now;
-
-            return category
-        })
+        // const now = this.now()
+        // store.getters.quiz.categories.list.forEach( (category, index, categories) => {
+        //     const prevCat = categories[index - 1]
+        //
+        //     if (prevCat && prevCat.end_at > now) {
+        //         category.is_active = false
+        //     } else category.is_active = category.end_at > now;
+        //
+        //     return category
+        // })
     }
 
     return {
