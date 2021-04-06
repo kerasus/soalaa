7fbf890ea0044d9666e5f4295956b5526eac3f78
Heliamh <helia.mhmdi@gmail.com>
2021-04-06 18:21:57 +0430

    resize test

--- src/pages/user/exam/participate/test.vue
+++ src/pages/user/exam/participate/test.vue
@@ -1,23 +1,28 @@
 <template>
 
+    <div @mousemove="resizing" @mouseup="endResize">
+        <div class="ghostbar" ref="ghostbar" v-show="dragging">
+        </div>
+        <div class="sidebar" ref="sidebar">
+            <span></span>
+            <div class="dragbar" ref="dragbar" @mousedown="startResize"></div>
+            sidebar
+        </div>
+        <div class="main" ref="main">
+            main
+        </div>
 
-    <div >
-       <v-row>
-               <VueDragResize  :h="600" :sticks="['mr']" :isDraggable="false" :isActive="false" :w="leftColumnWidth"  v-on:resizing="resizeLeftColumnWidth" >
-                   <p>{{ leftColumnWidth }} </p>
-               </VueDragResize>
-               <div style="height:600px;background-color: aqua" :style="{  width: (1600 - leftColumnWidth)+'px'}" >
-                   {{(600 - leftColumnWidth)}}
-               </div>
-       </v-row>
+
     </div>
 
 
 </template>
 
-<script>
 
-    import VueDragResize from 'vue-drag-resize'
+<script>
+    // import ElementQueries from 'css-element-queries/src/ElementQueries'
+    // import VueDragResize from 'vue-drag-resize'
+    import resize from 'vue-resize-directive'
 
 
     export default {
@@ -25,24 +30,105 @@
         data: () => ({
             leftColumnWidth: 500,
             rightColumnWidth: 200,
+            resizeCount: 0,
+            dragging: false
         }),
+        directives: {
+            resize,
+        },
         components: {
-            VueDragResize
+            // VueDragResize
+        },
+        computed: {
+            height() {
+                return this.$refs.main.clientHeight
+            },
+            top() {
+                return this.$refs.main.offsetTop
+            },
+            left() {
+                return this.$refs.main.offsetLeft
+            }
         },
         methods: {
+            startResize(e) {
+                e.preventDefault();
+                this.dragging = true;
 
-            resizeLeftColumnWidth(newRect) {
-                this.leftColumnWidth = newRect.width;
             },
+            resizing(e) {
+                e.preventDefault();
+
+                if (this.dragging){
+                    this.$refs.ghostbar.style.left = (e.pageX + 2) + 'px'
+                }
+
+
+            },
+            endResize(e) {
+                if (!this.dragging) {
+                    return
+                }
+
+                this.$refs.sidebar.style.width = (e.pageX + 2) + 'px'
+                this.$refs.main.style.left = (e.pageX + 2) + 'px'
+
+                // document.getElementById(document).unbind('mousemove');
+                this.dragging = false;
+            }
 
         },
-        created() {
-            this.leftColumnWidth = (window.innerWidth)/2
-        }
+
     }
 </script>
 
 <style scoped>
 
+    body, html {
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        margin: 0;
+    }
 
+    .main {
+        background-color: BurlyWood;
+        float: right;
+        position: absolute;
+        height: 200px;
+        right: 0;
+        left: 200px;
+        margin-top: 10px;
+
+    }
+
+    .sidebar {
+        background-color: IndianRed;
+
+        margin-top: 10px;
+        width: 200px;
+        float: left;
+        /*position: absolute;*/
+        height: 200px;
+        overflow-y: hidden;
+    }
+
+    .dragbar {
+        background-color: black;
+        height: 100%;
+        float: right;
+        width: 3px;
+        cursor: col-resize;
+    }
+
+    .ghostbar {
+        margin-top: 10px;
+        width: 3px;
+        background-color: #000;
+        opacity: 0.5;
+        cursor: col-resize;
+        z-index: 999;
+        position: absolute;
+        height: 200px;
+    }
 </style>
