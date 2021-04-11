9388e18292c777248c0d5f92816323f7c10f732a
kerasus <aliesmaeeli20@gmail.com>
2021-04-11 11:19:53 +0430

    upload images

--- src/pages/user/exam/List.vue
+++ src/pages/user/exam/List.vue
@@ -25,123 +25,125 @@
                                 عملیات
                             </v-col>
                         </v-row>
-                        <v-sheet
-                                v-for="item in exams.list"
-                                :key="item.id"
-                                class="mb-5 d-flex exam-list-sheet"
-                                elevation="0"
-                                outlined
-                                rounded
-                                shaped
-                        >
-                            <v-row class="table-row justify-center">
-                                <v-col cols="12" md="3" class="pr-7 justify-center">
-                                    {{ item.title }}
-                                </v-col>
-                                <v-col cols="12" md="2">
+                        <v-row v-for="item in exams.list" :key="item.id" >
+                            <v-col v-if="item.enable">
+                                <v-sheet
+                                        class="mb-5 d-flex exam-list-sheet"
+                                        elevation="0"
+                                        outlined
+                                        rounded
+                                        shaped
+                                >
+                                    <v-row class="table-row justify-center">
+                                        <v-col cols="12" md="3" class="pr-7 justify-center">
+                                            {{ item.title }}
+                                        </v-col>
+                                        <v-col cols="12" md="2">
                                     <span class="d-inline-block-md d-none">
                                         زمان شروع آزمون:
                                     </span>
-                                    {{ item.shamsiDate('start_at').dateTime }}
-                                </v-col>
-                                <v-col cols="12" md="2">
+                                            {{ item.shamsiDate('start_at').dateTime }}
+                                        </v-col>
+                                        <v-col cols="12" md="2">
                                     <span class="d-inline-block-md d-none">
                                         زمان پایان آزمون:
                                     </span>
-                                    {{ item.shamsiDate('finish_at').dateTime }}
-                                </v-col>
-                                <v-col cols="12" md="1">
+                                            {{ item.shamsiDate('finish_at').dateTime }}
+                                        </v-col>
+                                        <v-col cols="12" md="1">
                                     <span class="d-inline-block-md d-none">
                                         میزان تاخیر مجاز:
                                     </span>
-                                    {{ item.delay_time }}
-                                    دقیقه
-                                </v-col>
-                                <v-col cols="12" md="4">
-                                    <v-btn
+                                            {{ item.delay_time }}
+                                            دقیقه
+                                        </v-col>
+                                        <v-col cols="12" md="4">
+                                            <v-btn
 
-                                            color="#ffc107"
-                                            text
-                                            disabled
-                                    >
-                                        آزمون به پایان رسید
-                                    </v-btn>
-                                    <v-btn
-                                            v-if="!preventStartExam && item.user_exam_status === 'registered but participation time passed' || item.user_exam_status === 'registered but did not participate'"
-                                            :to="{ name: 'onlineQuiz.alaaView', params: { quizId: item.id, questNumber: 1 } }"
-                                            color="#ffc107"
-                                            text
-                                    >
-                                        شروع آزمون
-                                    </v-btn>
-                                    <v-btn
-                                            :to="{ name: 'onlineQuiz.alaaView', params: { quizId: item.id, questNumber: 1 } }"
-                                            color="#ffc107"
-                                            text
-                                    >
-                                        شروع آزمون
-                                    </v-btn>
-                                    <v-btn
-                                            v-if="!preventStartExam && item.user_exam_status === 'has participated and finished'"
-                                            @click="sendAnswersAndFinishExam(item)"
-                                            color="#ffc107"
-                                            text
-                                    >
-                                        ثبت پاسخنامه ذخیره شده در سیستم
-                                    </v-btn>
-                                    <v-btn
-                                            v-if="true || !preventStartExam && item.user_exam_status === 'has participated but not finished' || item.user_exam_status === 'has participated and finished' && false"
-                                            @click="continueExam(item.id)"
-                                            color="purple"
-                                            text
-                                    >
-                                        ادامه آزمون
-                                    </v-btn>
-                                    <v-btn
-                                            v-if="!preventStartExam && item.user_exam_status === 'registered but did not participate'"
-                                            :to="{ name: 'onlineQuiz.alaaView', params: { quizId: item.id, questNumber: 1 } }"
-                                            color="#00b5e6"
-                                            text
-                                    >
-                                        شروع آزمون
-                                    </v-btn>
-<!--                                    href="https://alaatv.com/landing/19"-->
-                                    <v-btn
-                                            v-if="!preventStartExam && item.user_exam_status === 'not registered' && false"
-                                            @click="registerExam(item.id)"
-                                            color="#00c753"
-                                            text
-                                    >
-                                        ثبت نام
-                                    </v-btn>
-                                    <v-btn
-                                            v-if="!preventStartExam && item.user_exam_status === 'not registered and registration time passed' && false"
-                                            @click="registerExam(item.id)"
-                                            color="#00c753"
-                                            text
-                                    >
-                                        ثبت نام
-<!--                                        اتمام مهلت ثبت نام-->
-                                    </v-btn>
-                                    <v-btn
-                                            v-if="item.user_exam_status === 'registered but not reached participation time'"
-                                            color="#00c753"
-                                            disabled
-                                            text
-                                    >
-                                        زمان آزمون فرا نرسیده
-                                    </v-btn>
-                                    <v-btn
-                                            v-if="item.user_exam_status === 'results recorded'"
-                                            color="#00b5e6"
-                                            text
-                                            :to="{ name: 'user.exam.results', params: { user_exam_id: item.user_exam_id, exam_id: item.id } }"
-                                    >
-                                        مشاهده نتایج
-                                    </v-btn>
-                                </v-col>
-                            </v-row>
-                        </v-sheet>
+                                                    color="#ffc107"
+                                                    text
+                                                    disabled
+                                            >
+                                                آزمون به پایان رسید
+                                            </v-btn>
+                                            <v-btn
+                                                    v-if="!preventStartExam && item.user_exam_status === 'registered but participation time passed' || item.user_exam_status === 'registered but did not participate'"
+                                                    :to="{ name: 'onlineQuiz.alaaView', params: { quizId: item.id, questNumber: 1 } }"
+                                                    color="#ffc107"
+                                                    text
+                                            >
+                                                شروع آزمون
+                                            </v-btn>
+                                            <v-btn
+                                                    :to="{ name: 'onlineQuiz.alaaView', params: { quizId: item.id, questNumber: 1 } }"
+                                                    color="#ffc107"
+                                                    text
+                                            >
+                                                شروع آزمون
+                                            </v-btn>
+                                            <v-btn
+                                                    v-if="!preventStartExam && item.user_exam_status === 'has participated and finished'"
+                                                    @click="sendAnswersAndFinishExam(item)"
+                                                    color="#ffc107"
+                                                    text
+                                            >
+                                                ثبت پاسخنامه ذخیره شده در سیستم
+                                            </v-btn>
+                                            <v-btn
+                                                    v-if="true || !preventStartExam && item.user_exam_status === 'has participated but not finished' || item.user_exam_status === 'has participated and finished' && false"
+                                                    @click="continueExam(item.id)"
+                                                    color="purple"
+                                                    text
+                                            >
+                                                ادامه آزمون
+                                            </v-btn>
+                                            <v-btn
+                                                    v-if="!preventStartExam && item.user_exam_status === 'registered but did not participate'"
+                                                    :to="{ name: 'onlineQuiz.alaaView', params: { quizId: item.id, questNumber: 1 } }"
+                                                    color="#00b5e6"
+                                                    text
+                                            >
+                                                شروع آزمون
+                                            </v-btn>
+                                            <!--                                    href="https://alaatv.com/landing/19"-->
+                                            <v-btn
+                                                    v-if="!preventStartExam && item.user_exam_status === 'not registered' && false"
+                                                    @click="registerExam(item.id)"
+                                                    color="#00c753"
+                                                    text
+                                            >
+                                                ثبت نام
+                                            </v-btn>
+                                            <v-btn
+                                                    v-if="!preventStartExam && item.user_exam_status === 'not registered and registration time passed' && false"
+                                                    @click="registerExam(item.id)"
+                                                    color="#00c753"
+                                                    text
+                                            >
+                                                ثبت نام
+                                                <!--                                        اتمام مهلت ثبت نام-->
+                                            </v-btn>
+                                            <v-btn
+                                                    v-if="item.user_exam_status === 'registered but not reached participation time'"
+                                                    color="#00c753"
+                                                    disabled
+                                                    text
+                                            >
+                                                زمان آزمون فرا نرسیده
+                                            </v-btn>
+                                            <v-btn
+                                                    v-if="item.user_exam_status === 'results recorded'"
+                                                    color="#00b5e6"
+                                                    text
+                                                    :to="{ name: 'user.exam.results', params: { user_exam_id: item.user_exam_id, exam_id: item.id } }"
+                                            >
+                                                مشاهده نتایج
+                                            </v-btn>
+                                        </v-col>
+                                    </v-row>
+                                </v-sheet>
+                            </v-col>
+                        </v-row>
                     </v-col>
                 </v-row>
             </v-col>
