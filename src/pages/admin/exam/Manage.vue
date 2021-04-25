0cafffe2463d9da81340a7fda97051e232542e12
neokazemi <neo.kazemi168@gmail.com>
2021-04-25 10:31:05 +0430

    manage.vue modal

--- src/pages/admin/exam/Manage.vue
+++ src/pages/admin/exam/Manage.vue
@@ -1,12 +1,23 @@
 <template>
     <v-container>
         <v-row>
-            <v-col col-lg="7" cols="12">
+            <v-col cols="12">
                 <exam-list @update-exam-id="updateExamId" ref="examList"/>
             </v-col>
-            <v-col>
-                <exam-info col-lg="5" cols="12" :exam="exam" @refresh-exam-list="rereshExamList"/>
-            </v-col>
+            <v-dialog v-model="dialog" max-width="600px">
+                <v-sheet>
+                    <v-row>
+                        <v-btn icon :style="{ margin: '20px' }" @click="dialog = false">
+                            <v-icon>mdi-close</v-icon>
+                        </v-btn>
+                    </v-row>
+                    <v-row>
+                        <v-col>
+                            <exam-info col-lg="5" cols="12" :exam="exam" @refresh-exam-list="rereshExamList"/>
+                        </v-col>
+                    </v-row>
+                </v-sheet>
+            </v-dialog>
         </v-row>
     </v-container>
 </template>
@@ -19,7 +30,8 @@
     export default {
         name: 'Manage',
         data: () => ({
-            exam: new Exam()
+            exam: new Exam(),
+            dialog: false,
         }),
         components: {ExamList, ExamInfo},
         methods: {
@@ -28,6 +40,7 @@
             },
             updateExamId(e){
                 this.exam = new Exam(e)
+                this.dialog = true
             }
         }
     }
