361e3dcd041836044e6c3f11702f409cc0730f36
kerasus <aliesmaeeli20@gmail.com>
2021-04-27 10:11:15 +0430

    refactor getUserQuestionData

--- src/pages/user/exam/participate/AlaaView.vue
+++ src/pages/user/exam/participate/AlaaView.vue
@@ -21,15 +21,15 @@
                                 </div>
                                 <div class="question-buttons">
                                     <v-btn icon @click="changeStatus(currentQuestion.id, 'o')">
-                                        <v-icon v-if="!getUserQuestionData() || getUserQuestionData().status !== 'o'" color="#888" size="30">mdi-checkbox-blank-circle-outline</v-icon>
-                                        <v-icon v-if="getUserQuestionData() && getUserQuestionData().status === 'o'" color="--primary-2" :size="30">mdi-checkbox-blank-circle</v-icon>
+                                        <v-icon v-if="!getUserQuestionData(quiz.id, currentQuestion.id) || getUserQuestionData(quiz.id, currentQuestion.id).status !== 'o'" color="#888" size="30">mdi-checkbox-blank-circle-outline</v-icon>
+                                        <v-icon v-if="getUserQuestionData(quiz.id, currentQuestion.id) && getUserQuestionData(quiz.id, currentQuestion.id).status === 'o'" color="--primary-2" :size="30">mdi-checkbox-blank-circle</v-icon>
                                     </v-btn>
                                     <v-btn icon @click="changeStatus(currentQuestion.id, 'x')">
-                                        <v-icon :color="getUserQuestionData() && getUserQuestionData().status === 'x' ? 'red' : '#888'" :size="30">mdi-close</v-icon>
+                                        <v-icon :color="getUserQuestionData(quiz.id, currentQuestion.id) && getUserQuestionData(quiz.id, currentQuestion.id).status === 'x' ? 'red' : '#888'" :size="30">mdi-close</v-icon>
                                     </v-btn>
                                     <v-btn icon @click="changeBookmark(currentQuestion.id)">
-                                        <v-icon v-if="!getUserQuestionData() || !getUserQuestionData().bookmarked" :size="30" color="#888">mdi-bookmark-outline</v-icon>
-                                        <v-icon v-if="getUserQuestionData() && getUserQuestionData().bookmarked" color="--accent-1" :size="30">mdi-bookmark</v-icon>
+                                        <v-icon v-if="!getUserQuestionData(quiz.id, currentQuestion.id) || !getUserQuestionData(quiz.id, currentQuestion.id).bookmarked" :size="30" color="#888">mdi-bookmark-outline</v-icon>
+                                        <v-icon v-if="getUserQuestionData(quiz.id, currentQuestion.id) && getUserQuestionData(quiz.id, currentQuestion.id).bookmarked" color="--accent-1" :size="30">mdi-bookmark</v-icon>
                                     </v-btn>
                                 </div>
                             </v-row>
@@ -125,16 +125,6 @@
                 })
         },
         methods: {
-            getUserQuestionData () {
-                if (
-                    !this.quiz.id ||
-                    !this.currentQuestion.id ||
-                    !this.userQuizListData[this.quiz.id]
-                ) {
-                    return false
-                }
-                return this.userQuizListData[this.quiz.id][this.currentQuestion.id]
-            },
             changeAppBarAndDrawer (state) {
                 this.$store.commit('AppLayout/updateAppBarAndDrawer', state)
             },
