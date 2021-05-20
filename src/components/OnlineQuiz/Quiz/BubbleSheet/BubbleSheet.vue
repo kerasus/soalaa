3f8e4feea332c34703a636c5e8cef8899c354120
kerasus <aliesmaeeli20@gmail.com>
2021-05-20 13:04:20 +0430

    show selected time in exam result page

--- src/components/OnlineQuiz/Quiz/BubbleSheet/BubbleSheet.vue
+++ src/components/OnlineQuiz/Quiz/BubbleSheet/BubbleSheet.vue
@@ -34,7 +34,20 @@
                         :style="{ width: '24%', cursor: 'pointer' }"
                         @click="ClickQuestionNumber(question.id)"
                 >
-                    {{ getQuestionNumberFromId(question.id) }}
+                    <v-tooltip v-if="getUserQuestionData(question.id)" bottom>
+                        <template v-slot:activator="{ on, attrs }">
+                              <span
+                                  v-bind="attrs"
+                                  v-on="on"
+                              >
+                              {{ getQuestionNumberFromId(question.id) }}
+                              </span>
+                        </template>
+                        <span>{{ showAnsweredAt(getUserQuestionData(question.id).answered_at) }}</span>
+                    </v-tooltip>
+                    <span v-else>
+                        {{ getQuestionNumberFromId(question.id) }}
+                    </span>
                 </div>
                 <div
                         v-for="choice in question.choices.list"
@@ -62,144 +75,178 @@
 </template>
 
 <script>
-    import {mixinQuiz, mixinUserActionOnQuestion} from "@/mixin/Mixins";
+    import moment from 'moment-jalaali'
+  import {mixinQuiz, mixinUserActionOnQuestion} from "@/mixin/Mixins";
 
-    export default {
-        name: 'BubbleSheet',
-        mixins: [mixinQuiz, mixinUserActionOnQuestion],
-        computed: {
-
-
-            bubbleSize() {
-
-                return this.$store.getters['AppLayout/bubbleSize']
-
-            },
-
-
-            boxHeight() {
-                const boxSize = this.bubbleSheetWidth - 24
-                // each group width is 140px
-                const horizontalGroupAmounts = Math.floor(boxSize / 140)
-                const verticalGroupAmount = Math.ceil(this.questionsInGroups.length / horizontalGroupAmounts)
-                return verticalGroupAmount * 182 + 20
-            },
-
-
-            questionsInGroups() {
-                let groups = [],
-                    chunk = 10
-                let array
-                if (this.questions === null) {
-                    array = this.getCurrentExamQuestionsInArray()
-                    for (let i = 0, j = array.length; i < j; i += chunk) {
-                        groups.push(array.slice(i, i + chunk))
-                    }
-                } else {
-                    array = this.questions
-                    for (let i = 0, j = 200; i < j; i += chunk) {
-                        groups.push(array.slice(i, i + chunk))
-                    }
-                }
-
-                return groups
-            }
-        },
-        props: {
-            bubbleSheetWidth: {
-                default: null
-            },
-            questions: {
-                default: null
-            },
-            info: {
-                default: null
-            },
-            exam: {
-                default: null
-            },
-            delayTime: {
-                default: 2000
-            },
-
-        },
-        data: () => ({
-            overlay: false,
-            boxSize: 600
-        }),
-        methods: {
-            getUserQuestionData (question_id) {
-                if (typeof question_id === 'undefined') {
-                    question_id = this.currentQuestion.id
-                }
-                if (!this.quiz.id || !question_id || !this.userQuizListData[this.quiz.id]) {
-                    return false
-                }
-                return this.userQuizListData[this.quiz.id][question_id]
-            },
-            changeWidth() {
-                // console.log('test')
-                this.$refs.bubbleSheet.style.height = this.questionListHeight() - 24 + 'px'
-            },
-            AnswerClicked(payload) {
-                if (this.info.type !== 'pasokh-nameh') {
-                    this.answerClicked(payload)
-                    this.clickChoice(payload.questionId)
-                }
-            },
-            ClickQuestionNumber(questionId) {
-                if (this.info.type !== 'pasokh-nameh') {
-                    this.clickQuestionNumber(questionId)
-                }
-            },
-            clickChoice(questionId) {
-                this.$emit('clickChoice', questionId)
-            },
-            clickQuestionNumber(questionId) {
-                this.$emit('scrollTo', questionId)
-            },
-            questionListHeight() {
-                // box is a col-7 with 12px padding
-                this.boxSize = this.$refs.bubbleSheet.clientWidth - 24
-                console.log('box', this.boxSize)
-                // each group width is 140px
-                const horizontalGroupAmounts = Math.floor(this.boxSize / 140)
-                const verticalGroupAmount = Math.ceil(this.questionsInGroups.length / horizontalGroupAmounts)
-                return verticalGroupAmount * 185 + 24
-            },
-            questionListPadding() {
-                const boxSize = this.$refs.bubbleSheet.clientWidth - 24
-                const horizontalGroupAmounts = (this.$refs.bubbleSheet.clientHeight - 8) / 182
-                const verticalGroupAmounts = Math.ceil(this.questionsInGroups.length / horizontalGroupAmounts)
-                return (boxSize - (verticalGroupAmounts * 140)) / 2 + 5
-            },
-            test () {
-                this.$refs.bubbleSheet.style.height = this.questionListHeight() - 24 + 'px'
-            }
-        },
-        created() {
-            if (this.delayTime === 0) {
-                this.overlay = true
-            }
-            // console.log(this.questions)
-            // console.log(this.getCurrentExamQuestionsInArray())
-        },
-        mounted() {
-            let that = this
-            setTimeout(() => {
-                if (that.$refs.bubbleSheet) {
-                    that.$refs.bubbleSheet.style.height = that.questionListHeight() - 24 + 'px'
-                }
-                // $('.questions-list').height(this.questionListHeight())
-                that.overlay = false
-            }, this.delayTime)
-        },
-        'windowSize.x': function () {
-            // this.$refs.bubbleSheet.offsetHeight()
-            this.$refs.bubbleSheet.style.height = this.questionListHeight() - 24 + 'px'
-            // $('.questions-list').height(this.questionListHeight())
+  export default {
+    name: 'BubbleSheet',
+    mixins: [
+      mixinQuiz,
+      mixinUserActionOnQuestion
+    ],
+    computed: {
+      bubbleSize() {
+        return this.$store.getters['AppLayout/bubbleSize']
+      },
+      boxHeight() {
+        const boxSize = this.bubbleSheetWidth - 24
+        // each group width is 140px
+        const horizontalGroupAmounts = Math.floor(boxSize / 140)
+        const verticalGroupAmount = Math.ceil(this.questionsInGroups.length / horizontalGroupAmounts)
+        return verticalGroupAmount * 182 + 20
+      },
+      questionsInGroups() {
+        let groups = [],
+            chunk  = 10
+        let array
+        if (this.questions === null)
+        {
+          array = this.getCurrentExamQuestionsInArray()
+          for (let i = 0, j = array.length; i < j; i += chunk)
+          {
+            groups.push(array.slice(i, i + chunk))
+          }
+        } else
+        {
+          array = this.questions
+          for (let i = 0, j = 200; i < j; i += chunk)
+          {
+            groups.push(array.slice(i, i + chunk))
+          }
         }
+        return groups
+      }
+    },
+    props: {
+      bubbleSheetWidth: {
+        default: null
+      },
+      questions: {
+        default: null
+      },
+      info: {
+        default: null
+      },
+      exam: {
+        default: null
+      },
+      delayTime: {
+        default: 2000
+      },
+    },
+    data: () => ({
+      showDateOfAnsweredAt: false,
+      overlay: false,
+      boxSize: 600
+    }),
+    methods: {
+      showAnsweredAt (answeredAt) {
+        let formatString = 'HH:mm:ss'
+        if (this.showDateOfAnsweredAt) {
+          formatString = 'HH:mm:ss jYYYY/jMM/jDD'
+        }
+
+        return moment(new Date(answeredAt)).format(formatString)
+      },
+      checkForShowDateOfAnsweredAt () {
+        let dateTime = ''
+        let that = this
+        this.questionsInGroups.forEach( groupItem => {
+          groupItem.forEach( questionItem => {
+            const userQuestionData = that.getUserQuestionData(questionItem.id)
+            if (userQuestionData && userQuestionData.answered_at) {
+              if (dateTime === '') {
+                dateTime = moment(new Date(userQuestionData.answered_at))
+              } else {
+                let dayDiff = dateTime.diff(moment(new Date(userQuestionData.answered_at)), 'days')
+                if (dayDiff !== 0) {
+                  that.showDateOfAnsweredAt = true
+                }
+              }
+            }
+          })
+        })
+      },
+      getUserQuestionData(question_id) {
+        if (typeof question_id === 'undefined')
+        {
+          question_id = this.currentQuestion.id
+        }
+        if (!this.quiz.id || !question_id || !this.userQuizListData[this.quiz.id])
+        {
+          return false
+        }
+        return this.userQuizListData[this.quiz.id][question_id]
+      },
+      changeWidth() {
+        // console.log('test')
+        this.$refs.bubbleSheet.style.height = this.questionListHeight() - 24 + 'px'
+      },
+      AnswerClicked(payload) {
+        if (this.info.type !== 'pasokh-nameh')
+        {
+          this.answerClicked(payload)
+          this.clickChoice(payload.questionId)
+        }
+      },
+      ClickQuestionNumber(questionId) {
+        if (this.info.type !== 'pasokh-nameh')
+        {
+          this.clickQuestionNumber(questionId)
+        }
+      },
+      clickChoice(questionId) {
+        this.$emit('clickChoice', questionId)
+      },
+      clickQuestionNumber(questionId) {
+        this.$emit('scrollTo', questionId)
+      },
+      questionListHeight() {
+        // box is a col-7 with 12px padding
+        this.boxSize = this.$refs.bubbleSheet.clientWidth - 24
+        console.log('box', this.boxSize)
+        // each group width is 140px
+        const horizontalGroupAmounts = Math.floor(this.boxSize / 140)
+        const verticalGroupAmount = Math.ceil(this.questionsInGroups.length / horizontalGroupAmounts)
+        return verticalGroupAmount * 185 + 24
+      },
+      questionListPadding() {
+        const boxSize = this.$refs.bubbleSheet.clientWidth - 24
+        const horizontalGroupAmounts = (this.$refs.bubbleSheet.clientHeight - 8) / 182
+        const verticalGroupAmounts = Math.ceil(this.questionsInGroups.length / horizontalGroupAmounts)
+        return (boxSize - (verticalGroupAmounts * 140)) / 2 + 5
+      },
+      test() {
+        this.$refs.bubbleSheet.style.height = this.questionListHeight() - 24 + 'px'
+      }
+    },
+    created() {
+      if (this.delayTime === 0)
+      {
+        this.overlay = true
+      }
+      // console.log(this.questions)
+      // console.log(this.getCurrentExamQuestionsInArray())
+    },
+    mounted() {
+      let that = this
+      setTimeout(() => {
+        if (that.$refs.bubbleSheet)
+        {
+          that.$refs.bubbleSheet.style.height = that.questionListHeight() - 24 + 'px'
+        }
+        // $('.questions-list').height(this.questionListHeight())
+        that.overlay = false
+      }, this.delayTime)
+
+      this.checkForShowDateOfAnsweredAt()
+    },
+    'windowSize.x': function () {
+      // this.$refs.bubbleSheet.offsetHeight()
+      this.$refs.bubbleSheet.style.height = this.questionListHeight() - 24 + 'px'
+      // $('.questions-list').height(this.questionListHeight())
     }
+  }
 </script>
 
 <style scoped>
