2c906900b17e21b6081ced74d16b70a444cce24f
JasGH <yasaman.gonehi@gmail.com>
2021-06-14 13:08:48 +0430

    working on log

--- src/components/QuestionBank/EditQuestion/Log/LogItem/LogItem.vue
+++ src/components/QuestionBank/EditQuestion/Log/LogItem/LogItem.vue
@@ -1,27 +1,46 @@
 <template>
-  <div>
+  <div class="log-item">
     <v-container>
-      <div>
-      به‌روزشده توسط
-    {{ logData.editor }}
-      در
-      {{logData.date}}
-    <v-icon class="ml-4">
-      mdi-eye-outline
-    </v-icon>
-      </div>
-      <div>
-        وضعیت از {{logData.previousStatus}} به {{logData.currentStatus}}
-      </div>
-      <div v-for="(item, index) in logData.commnets" :key="index">
-        {{ item.author}}
-        <div>
-          {{ item.text}}
-        </div>
-        <div>
-          {{ item.date}}
-        </div>
-      </div>
+      <v-row no-gutters>
+        <v-col cols="1">
+          <v-icon>
+            mdi-circle-medium
+          </v-icon>
+        </v-col>
+        <v-col>
+          <v-row justify="space-between">
+            <v-col cols="10">
+              <div>
+                به‌روزشده توسط
+                <span class="log-editor">{{ logData.editor }}</span>
+                در
+                <span>{{ logData.date }}</span>
+              </div>
+            </v-col>
+            <v-col class="text-left">
+              <v-icon class="eye-icon">
+                mdi-eye-outline
+              </v-icon>
+            </v-col>
+          </v-row>
+          <div>
+            وضعیت از
+            <v-chip color="orange" text-color="white">{{ logData.previousStatus }}</v-chip>
+            به
+            <v-chip color="red" text-color="white">{{ logData.currentStatus }}</v-chip>
+            تغییر یافت.
+          </div>
+          <div v-for="(item, index) in logData.commnets" :key="index">
+            :{{ item.author }}
+            <div>
+              {{ item.text }}
+            </div>
+            <div>
+              {{ item.date }}
+            </div>
+          </div>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -34,11 +53,27 @@ export default {
     }
   },
   name: "logItem",
-  data (){
+  data() {
     return {}
   }
 }
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.log-item {
+  font-size: 12px;
+}
+
+.log-editor {
+  color: #4a94dc;
+}
+.v-chip.v-size--default {
+  font-size: 12px;
+  height: 20px;
+}
+/*.eye-icon{*/
+/*  width: 12px;*/
+/*  height: 12px;*/
+/*}*/
+</style>
