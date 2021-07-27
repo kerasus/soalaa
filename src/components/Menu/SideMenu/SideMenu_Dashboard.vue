afec83d9346a518a7b2eb14cde1415c71b73a0ca
kerasus <aliesmaeeli20@gmail.com>
2021-07-27 17:48:42 +0430

    side menu bug fix

--- src/components/Menu/SideMenu/SideMenu_Dashboard.vue
+++ src/components/Menu/SideMenu/SideMenu_Dashboard.vue
@@ -38,9 +38,7 @@
         :class="{'router-link-active': $route.name === item.to.name}"
         :to="item.to"
       >
-        <v-list-item
-          :class="{ 'v-list-item--active': $route.name === item.to.name, 'v-list-item--link': $route.name === item.to.name, 'deep-purple--text': $route.name === item.to.name }"
-        >
+        educationList <v-list-item :class="{ 'v-list-item--active': $route.name === item.to.name, 'v-list-item--link': $route.name === item.to.name, 'deep-purple--text': $route.name === item.to.name }">
           <v-list-item-title>{{ item.displayName }}</v-list-item-title>
         </v-list-item>
       </router-link>
@@ -105,9 +103,7 @@
         :class="{'router-link-active': $route.name === item.to.name}"
         :to="item.to"
       >
-        <v-list-item
-          :class="{ 'v-list-item--active': $route.name === item.to.name, 'v-list-item--link': $route.name === item.to.name, 'deep-purple--text': $route.name === item.to.name }"
-        >
+        <v-list-item :class="{ 'v-list-item--active': $route.name === item.to.name, 'v-list-item--link': $route.name === item.to.name, 'deep-purple--text': $route.name === item.to.name }">
           <v-list-item-title>{{ item.displayName }}</v-list-item-title>
         </v-list-item>
       </router-link>
@@ -132,20 +128,17 @@
         :class="{'router-link-active': $route.name === item.to.name}"
         :to="item.to"
       >
-        <v-list-item
-          :class="{ 'v-list-item--active': $route.name === item.to.name, 'v-list-item--link': $route.name === item.to.name, 'deep-purple--text': $route.name === item.to.name }"
-        >
+        <v-list-item :class="{ 'v-list-item--active': $route.name === item.to.name, 'v-list-item--link': $route.name === item.to.name, 'deep-purple--text': $route.name === item.to.name }">
           <v-list-item-title>{{ item.displayName }}</v-list-item-title>
         </v-list-item>
       </router-link>
       <v-list-group
+        v-if="user.has_admin_permission"
         class="question_bank"
         prepend-icon="mdi-bank-outline"
         color="white"
       >
-        <template
-          v-slot:activator
-        >
+        <template v-slot:activator>
           <v-list-item-title>
             بانک سوال
           </v-list-item-title>
@@ -176,92 +169,91 @@
 </template>
 
 <script>
-  import {mixinAuth} from '@/mixin/Mixins'
+import { mixinAuth } from '@/mixin/Mixins'
 
-  export default {
-    name: "SideMenuDashboard",
-    mixins: [mixinAuth],
-    data: () => ({
-      userGroup: null,
-      adminGroup: null,
-      userList: [
-        {
-          displayName: 'آزمون های سه آ',
-          to: {name: 'user.exam.list'}
-        },
-        {
-          displayName: 'سوالات متداول',
-          to: {name: 'faq'}
-        }
-      ],
-      adminList: [
-        {
-          displayName: 'ویرایش درخت دانش',
-          to: {name: 'tree.edit'}
-        },
-        {
-          displayName: 'ساخت سوال',
-          to: {name: 'quest.create'}
-        },
-        {
-          displayName: 'لیست آزمون ها',
-          to: {name: 'onlineQuiz.exams'}
-        },
-        {
-          displayName: 'ثبت ویدئو تحلیل',
-          to: {name: 'video.set'}
-        },
-        {
-          displayName: 'بانک سوال',
-          to: {name: 'quest.index'}
-        },
-        {
-          displayName: 'لیست دروس',
-          to: {name: 'subCategory.edit'}
-        },
-        {
-          displayName: 'لیست دفترچه ها',
-          to: {name: 'category.edit'}
-        },
-      ],
-      educationList: [
-        {
-          displayName: 'لیست آزمون ها',
-          to: {name: 'onlineQuiz.exams'}
-        },
-      ]
-    }),
-    created() {
-      if (
-              !this.user.has_admin_permission &&
-              this.user.has_educational_permission
-      )
+export default {
+  name: "SideMenuDashboard",
+  mixins: [mixinAuth],
+  data: () => ({
+    userGroup: null,
+    adminGroup: null,
+    userList: [
       {
-        this.adminList = [
-          {
-            displayName: 'لیست آزمون ها',
-            to: {name: 'onlineQuiz.exams'}
-          }
-        ]
+        displayName: 'آزمون های سه آ',
+        to: { name: 'user.exam.list' }
+      },
+      {
+        displayName: 'سوالات متداول',
+        to: { name: 'faq' }
       }
+    ],
+    adminList: [
+      {
+        displayName: 'ویرایش درخت دانش',
+        to: { name: 'tree.edit' }
+      },
+      {
+        displayName: 'ساخت سوال',
+        to: { name: 'quest.create' }
+      },
+      {
+        displayName: 'لیست آزمون ها',
+        to: { name: 'onlineQuiz.exams' }
+      },
+      {
+        displayName: 'ثبت ویدئو تحلیل',
+        to: { name: 'video.set' }
+      },
+      {
+        displayName: 'بانک سوال',
+        to: { name: 'quest.index' }
+      },
+      {
+        displayName: 'لیست دروس',
+        to: { name: 'subCategory.edit' }
+      },
+      {
+        displayName: 'لیست دفترچه ها',
+        to: { name: 'category.edit' }
+      },
+    ],
+    educationList: [
+      // {
+      //   displayName: 'لیست آزمون ها',
+      //   to: { name: 'onlineQuiz.exams' }
+      // },
+    ]
+  }),
+  created () {
+    if (
+      !this.user.has_admin_permission &&
+      this.user.has_educational_permission
+    ) {
+      this.adminList = [
+        {
+          displayName: 'لیست آزمون ها',
+          to: { name: 'onlineQuiz.exams' }
+        }
+      ]
     }
   }
+}
 </script>
 <style scoped>
 .v-list__group__header__prepend-icon .v-icon {
   color: red;
 }
-    .admin-panel {
-        border-top: 1px solid var(--surface-1);
-    }
-    .menu .v-list-item-group .question_bank{
-      padding: 0;
-    }
-    .menu .v-list-item-group .question_bank .question_bank_item_list{
-      margin-right: 30px;
-      font-size: 14px !important;
-    }
-.v-list .v-list-item--active .v-icon{
+.admin-panel {
+  border-top: 1px solid var(--surface-1);
+}
+.menu .v-list-item-group .question_bank {
+  padding: 0;
+}
+.menu .v-list-item-group .question_bank .question_bank_item_list {
+  margin-right: 30px;
+  font-size: 14px !important;
+}
+.v-list .v-list-item--active .v-icon {
   color: white;
 }
 </style>
@@ -271,33 +263,38 @@
   content: "\F0140";
   color: white;
 }
-    .menu .v-list-item__title {
-        color: var(--text-3);
-        font-size: 16px !important;
-        font-weight: normal !important;
-    }
+.menu .v-list-item__title {
+  color: var(--text-3);
+  font-size: 16px !important;
+  font-weight: normal !important;
+}
 
-    .menu .v-list-item__icon {
-        margin-left: 12px !important;
-    }
+.menu .v-list-item__icon {
+  margin-left: 12px !important;
+}
 
-    .v-list.menu {
-        padding: 8px 0 !important;
-    }
+.v-list.menu {
+  padding: 8px 0 !important;
+}
 
-    .router-link-exact-active .v-list-item {
-        /*background: var(--primary-1);*/
-        padding: 0 8px 0 0 !important;
-        border-radius: 0 !important;
-    }
+.router-link-exact-active .v-list-item {
+  /*background: var(--primary-1);*/
+  padding: 0 8px 0 0 !important;
+  border-radius: 0 !important;
+}
 
-    .router-link-exact-active .v-list-item .v-list-item__title {
-        border-left: 4px solid var(--surface-1);
-    }
-    .v-list-group--active > .v-list-group__header .v-list-item, .v-list-group--active > .v-list-group__header .v-list-item__content, .v-list-group--active > .v-list-group__header .v-list-group__header__prepend-icon .v-icon {
-      color: white;
-    }
-    .menu .theme--light.v-icon {
-      color: white !important;
-    }
+.router-link-exact-active .v-list-item .v-list-item__title {
+  border-left: 4px solid var(--surface-1);
+}
+.v-list-group--active > .v-list-group__header .v-list-item,
+.v-list-group--active > .v-list-group__header .v-list-item__content,
+.v-list-group--active
+  > .v-list-group__header
+  .v-list-group__header__prepend-icon
+  .v-icon {
+  color: white;
+}
+.menu .theme--light.v-icon {
+  color: white !important;
+}
 </style>
