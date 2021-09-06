017990201153eb7540e08254f6d86a017b01fcb2
Mostafa <neo.kazemi168@gmail.com>
2021-09-06 17:43:34 +0430

    poem

--- src/components/Question/questionField.vue
+++ src/components/Question/questionField.vue
@@ -9,7 +9,7 @@
         :loading="loading"
         :access-token="$store.getters['Auth/accessToken']"
         :upload-url="imageUrl"
-        :options="{ bubbleMenu: false, floatingMenu: false }"
+        :options="{ bubbleMenu: false, floatingMenu: false, poem: true }"
       />
     </v-col>
     <!-- eslint-disable vue/no-v-html -->
--- src/components/VueKatex.vue
+++ src/components/VueKatex.vue
@@ -31,6 +31,42 @@ export default {
 }
 </script>
 
+<style>
+  .beit {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
+  }
+  .beit .mesra {
+    position: relative;
+    width: 100%;
+    min-height: 1px;
+    padding-right: 15px;
+    padding-left: 15px;
+    -ms-flex-preferred-size: 0;
+    flex-basis: 0;
+    -webkit-box-flex: 1;
+    -ms-flex-positive: 1;
+    flex-grow: 1;
+    max-width: 100%;
+    white-space: nowrap;
+  }
+
+  @media only screen and (max-width: 500px) {
+    .beit {
+      flex-direction: column;
+    }
+    .beit .mesra {
+      white-space: normal;
+      flex-basis: auto;
+    }
+  }
+</style>
+
 <style scoped lang="scss">
 .html-katex {
   table {
