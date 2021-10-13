604b5cc6cb8162d0214d48249ee752ee1c91cbcc
JasGH <yasaman.gonehi@gmail.com>
2021-10-13 13:58:25 +0330

    added modals

---
+++ src/models/AlaaSet.js
@@ -0,0 +1,26 @@
+import { Model } from 'js-abstract-model'
+import { AlaaContentList } from '../models/AlaaContent'
+import API_ADDRESS from '../api/Addresses'
+
+class AlaaSet extends Model {
+    constructor(data) {
+        super(data, [
+            {
+                key: 'baseRoute',
+                default: API_ADDRESS.set.base
+            },
+            {key: 'id'},
+            {key: 'title'},
+            {key: 'short_title'},
+            {key: 'photo'},
+            {key: 'author'},
+            {key: 'contents_count'},
+            {
+                key: 'contents',
+                relatedModel: AlaaContentList
+            }
+        ])
+    }
+}
+
+export { AlaaSet }
---
+++ src/models/QuestSubcategory.js
@@ -0,0 +1,37 @@
+import { Model, Collection } from 'js-abstract-model'
+import {QuestionList} from '@/models/Question'
+import API_ADDRESS from "@/api/Addresses";
+
+class QuestSubcategory extends Model {
+    constructor (data) {
+        super(data, [
+            {
+                key: 'baseRoute',
+                default: API_ADDRESS.questionSubcategory.base
+            },
+            { key: 'id' },
+            { key: 'title' },
+            {
+                key: 'permissions',
+                default: {
+                    insert: false,
+                    view: false
+                }
+            },
+            { key: 'order' },
+            { key: 'category_id' },
+            {
+                key: 'questions',
+                relatedModel: QuestionList
+            },
+        ])
+    }
+}
+
+class QuestSubcategoryList extends Collection {
+    model () {
+        return QuestSubcategory
+    }
+}
+
+export { QuestSubcategory, QuestSubcategoryList }
