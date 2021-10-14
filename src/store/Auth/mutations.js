09b7a8fdb44037f32625e07c84ce53e2c3eb4a94
mitraKhm <mitra.kh1995@gmail.com>
2021-10-14 14:53:10 +0330

    fix action

--- src/store/Auth/mutations.js
+++ src/store/Auth/mutations.js
@@ -2,6 +2,7 @@ export function updateUser (state, newInfo) {
   state.user = newInfo
 }
 export function updateAccessToken (state, newInfo) {
+  console.log('mue')
   state.accessToken = newInfo
 }
 export function updateRedirectTo (state, newInfo) {
