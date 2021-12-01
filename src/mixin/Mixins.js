6374a9bc04da7c6d39f429d7ef2219f709062b52
JasGH <yasaman.gonehi@gmail.com>
2021-12-01 11:38:44 +0330

    alaa view

--- src/mixin/Mixins.js
+++ src/mixin/Mixins.js
@@ -1,6 +1,6 @@
 import mixinAuth from '../mixin/Auth'
 import mixinWindowSize from '../mixin/WindowSize'
 import mixinQuiz from '../mixin/Quiz'
+import mixinDrawer from '../mixin/Drawer'
 import mixinUserActionOnQuestion from '../mixin/UserActionOnQuestion'
-
-export { mixinAuth, mixinWindowSize, mixinQuiz, mixinUserActionOnQuestion }
+export { mixinAuth, mixinWindowSize, mixinQuiz, mixinDrawer, mixinUserActionOnQuestion }
