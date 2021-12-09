d348f5640c13e32c1bd7a166b6829d6df13b9589
mitraKhm <mitra.kh1995@gmail.com>
2021-12-09 10:59:10 +0330

    exam list - bug fix

--- src/components/OnlineQuiz/Quiz/ExamList.vue
+++ src/components/OnlineQuiz/Quiz/ExamList.vue
@@ -169,7 +169,7 @@
                   v-ripple:yellow
                   clickable
                   manual-focus
-                  :to="{ name: 'exam.results', params: { examId: tableRow.id}}"
+                  @click="goExamResult(tableRow.id)"
                 >
                   <q-item-section>نتایج تمام شرکت کنندگان</q-item-section>
                 </q-item>
@@ -256,6 +256,14 @@ export default {
     this.getExams()
   },
   methods: {
+    goExamResult (id) {
+      this.$router.push({
+        name: 'exam.results',
+        params: {
+          examId: id
+        }
+      })
+    },
     getExams () {
       this.$store.dispatch('loading/linearLoading', true)
       this.$axios.get(API_ADDRESS.exam.base(this.pagination.page))
