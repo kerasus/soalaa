30d30a7ef417cdeb197dc34affd936b5fa863985
mitraKhm <mitra.kh1995@gmail.com>
2021-12-11 08:58:43 +0330

    add exam list to home page

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('pages/User/exam/List'),
     children: [
       {
         path: '',
