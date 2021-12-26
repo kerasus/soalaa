59c7db4962a20b6fb9a0e4cd89e50d5d6f6c10a7
JasGH <yasaman.gonehi@gmail.com>
2021-12-26 15:46:08 +0330

    Result

--- src/components/OnlineQuiz/Quiz/resultTables/personalResult.vue
+++ src/components/OnlineQuiz/Quiz/resultTables/personalResult.vue
@@ -1,151 +1,151 @@
 <template>
-  <v-container
-    fluid
+  <div
     class="personal-results"
   >
-<!--    <v-row v-if="report">-->
-<!--      <v-col-->
-<!--        md="7"-->
-<!--        cols="12"-->
-<!--      >-->
-<!--        <v-row>-->
-<!--          <v-col>-->
-<!--            <v-data-table-->
-<!--              hide-default-footer-->
-<!--              :headers="headers1"-->
-<!--              :header-props="{sortByText: 'ترتیب'}"-->
-<!--              :items="report.sub_category"-->
-<!--              :items-per-page="15"-->
-<!--              class="elevation-1"-->
-<!--            >-->
-<!--              <template v-slot:top>-->
-<!--                <br>-->
-<!--                <span class="tableTitle">-->
-<!--                  جدول عملکرد دروس-->
-<!--                </span>-->
-<!--                <br>-->
-<!--                <br>-->
-<!--              </template>-->
-<!--            </v-data-table>-->
-<!--          </v-col>-->
-<!--        </v-row>-->
-<!--        <v-row>-->
-<!--          <v-col>-->
-<!--            <v-data-table-->
-<!--              hide-default-footer-->
-<!--              :headers="headers3"-->
-<!--              class="elevation-1 mb-2"-->
-<!--              :items="[report.exam_user]"-->
-<!--            >-->
+    <div
+      class="row"
+      v-if="report"
+    >
+      <div
+        class="col col-12 col-md-7"
+      >
+        <div class="row">
+          <div class="col">
+            <v-data-table
+              hide-default-footer
+              :headers="headers1"
+              :header-props="{sortByText: 'ترتیب'}"
+              :items="report.sub_category"
+              :items-per-page="15"
+              class="elevation-1"
+            >
+              <template v-slot:top>
+                <br>
+                <span class="tableTitle">
+                  جدول عملکرد دروس
+                </span>
+                <br>
+                <br>
+              </template>
+            </v-data-table>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <v-data-table
+              hide-default-footer
+              :headers="headers3"
+              class="elevation-1 mb-2"
+              :items="[report.exam_user]"
+            >
 <!--              <template v-slot:item.created_at="{ item }">-->
 <!--                {{ shamsiDate(item.created_at) }}-->
 <!--              </template>-->
 <!--              <template v-slot:item.accept_at="{ item }">-->
 <!--                {{ shamsiDate(item.accept_at) }}-->
 <!--              </template>-->
-<!--            </v-data-table>-->
-<!--          </v-col>-->
-<!--        </v-row>-->
-<!--      </v-col>-->
-<!--      <v-col-->
-<!--        md="5"-->
-<!--        cols="12"-->
-<!--      >-->
-<!--        <v-row>-->
-<!--          <v-col>-->
-<!--            <v-data-table-->
-<!--              hide-default-footer-->
-<!--              :headers="headers2"-->
-<!--              :header-props="{sortByText: 'ترتیب'}"-->
-<!--              :items="report.zirgorooh"-->
-<!--              :items-per-page="5"-->
-<!--              class="elevation-1"-->
-<!--            >-->
-<!--              <template v-slot:top>-->
-<!--                <br>-->
-<!--                <span class="tableTitle ">-->
-<!--                  نتیجه در زیر گروه ها-->
-<!--                </span>-->
-<!--                <br>-->
-<!--                <br>-->
-<!--              </template>-->
-<!--            </v-data-table>-->
-<!--          </v-col>-->
-<!--        </v-row>-->
-<!--        <v-row>-->
-<!--          <v-col class="card-col">-->
-<!--            <div class="card-parent elevation-1">-->
-<!--              <v-card :elevation="0">-->
-<!--                <v-card-title>-->
-<!--                  <v-row>-->
-<!--                    <v-col>-->
-<!--                      تعداد کل شرکت کنندگان-->
-<!--                    </v-col>-->
-<!--                    <v-col>-->
-<!--                      ماکزمیم تراز کل زیر گروه-->
-<!--                    </v-col>-->
-<!--                  </v-row>-->
-<!--                </v-card-title>-->
+            </v-data-table>
+          </div>
+        </div>
+      </div>
+      <div
+        class="col col-md-5 col-12"
+      >
+        <div class="row">
+          <div class="col">
+            <v-data-table
+              hide-default-footer
+              :headers="headers2"
+              :header-props="{sortByText: 'ترتیب'}"
+              :items="report.zirgorooh"
+              :items-per-page="5"
+              class="elevation-1"
+            >
+              <template v-slot:top>
+                <br>
+                <span class="tableTitle ">
+                  نتیجه در زیر گروه ها
+                </span>
+                <br>
+                <br>
+              </template>
+            </v-data-table>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col card-col">
+            <div class="card-parent elevation-1">
+              <v-card :elevation="0">
+                <v-card-title>
+                  <div class="row">
+                    <div class="col">
+                      تعداد کل شرکت کنندگان
+                    </div>
+                    <div class="col">
+                      ماکزمیم تراز کل زیر گروه
+                    </div>
+                  </div>
+                </v-card-title>
 
-<!--                <span>-->
-<!--                  <v-row>-->
-<!--                    <v-col>-->
-<!--                      {{ report.n_normal_participants }}-->
-<!--                    </v-col>-->
-<!--                    <v-col>-->
-<!--                      {{ report.main.taraaz }}-->
-<!--                    </v-col>-->
-<!--                  </v-row>-->
-<!--                </span>-->
-<!--              </v-card>-->
-<!--            </div>-->
-<!--          </v-col>-->
-<!--        </v-row>-->
-<!--        <v-row>-->
-<!--          <v-col class="card-col">-->
-<!--            <div class="card-parent elevation-1">-->
-<!--              <v-card-->
-<!--                class="mb-2"-->
-<!--                :elevation="0"-->
-<!--              >-->
-<!--                <v-card-title>-->
-<!--                  <v-row>-->
-<!--                    <v-col :cols="(report.main.rank_school) ? 3 : 4">-->
-<!--                      رتبه کل کشوری-->
-<!--                    </v-col>-->
-<!--                    <v-col :cols="(report.main.rank_school) ? 3 : 4">-->
-<!--                      رتبه در استان-->
-<!--                    </v-col>-->
-<!--                    <v-col :cols="(report.main.rank_school) ? 3 : 4">-->
-<!--                      رتبه در شهر-->
-<!--                    </v-col>-->
-<!--                    <v-col-->
-<!--                      v-if="report.main.rank_school"-->
-<!--                      cols="3"-->
-<!--                    >-->
-<!--                      رتبه در مدرسه-->
-<!--                    </v-col>-->
-<!--                  </v-row>-->
-<!--                </v-card-title>-->
+                <span>
+                  <div class="row">
+                    <div class="col">
+                      {{ report.n_normal_participants }}
+                    </div>
+                    <div class="col">
+                      {{ report.main.taraaz }}
+                    </div>
+                  </div>
+                </span>
+              </v-card>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col card-col">
+            <div class="card-parent elevation-1">
+              <v-card
+                class="mb-2"
+                :elevation="0"
+              >
+                <v-card-title>
+                  <div class="row">
+                    <div class="col" :cols="(report.main.rank_school) ? 3 : 4">
+                      رتبه کل کشوری
+                    </div>
+                    <div class="col" :cols="(report.main.rank_school) ? 3 : 4">
+                      رتبه در استان
+                    </div>
+                    <div class="col" :cols="(report.main.rank_school) ? 3 : 4">
+                      رتبه در شهر
+                    </div>
+                    <div
+                      class="col col-3"
+                      v-if="report.main.rank_school"
+                    >
+                      رتبه در مدرسه
+                    </div>
+                  </div>
+                </v-card-title>
 
-<!--                <span>-->
-<!--                  <v-row>-->
-<!--                    <v-col :cols="(report.main.rank_school) ? 3 : 4">{{ report.main.rank_country }}</v-col>-->
-<!--                    <v-col :cols="(report.main.rank_school) ? 3 : 4">{{ report.main.rank_province }}</v-col>-->
-<!--                    <v-col :cols="(report.main.rank_school) ? 3 : 4">{{ report.main.rank_city }}</v-col>-->
-<!--                    <v-col-->
-<!--                      v-if="report.main.rank_school"-->
-<!--                      cols="3"-->
-<!--                    >{{ report.main.rank_school }}</v-col>-->
-<!--                  </v-row>-->
-<!--                </span>-->
-<!--              </v-card>-->
-<!--            </div>-->
-<!--          </v-col>-->
-<!--        </v-row>-->
-<!--      </v-col>-->
-<!--    </v-row>-->
-  </v-container>
+                <span>
+                  <div class="row">
+                    <div class="col" :cols="(report.main.rank_school) ? 3 : 4">{{ report.main.rank_country }}</div>
+                    <div class="col" :cols="(report.main.rank_school) ? 3 : 4">{{ report.main.rank_province }}</div>
+                    <div class="col" :cols="(report.main.rank_school) ? 3 : 4">{{ report.main.rank_city }}</div>
+                    <div
+                      class="col col-3"
+                      v-if="report.main.rank_school"
+                    >{{ report.main.rank_school }}</div>
+                  </div>
+                </span>
+              </v-card>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
