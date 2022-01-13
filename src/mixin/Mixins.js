15cb5065955456164e59cfbdaa58c0ccc10cfd88
Ali <aliesmaeeli20@gmail.com>
2022-01-13 14:38:21 +0330

    ready for quasar

--- src/mixin/Mixins.js
+++
@@ -1,7 +0,0 @@
-import mixinQuiz from '@/mixin/Quiz'
-import mixinDrawer from '@/mixin/Drawer'
-import mixinWindowSize from '@/mixin/WindowSize'
-import mixinUserActionOnQuestion from '@/mixin/UserActionOnQuestion'
-import mixinAuth from '@/mixin/Auth'
-
-export { mixinAuth, mixinQuiz, mixinDrawer, mixinWindowSize, mixinUserActionOnQuestion }
