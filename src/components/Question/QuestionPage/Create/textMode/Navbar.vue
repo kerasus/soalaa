5ab02fd7a2e5baa293ad02c1eac3bb1398dd9489
JasGH <yasaman.gonehi@gmail.com>
2022-03-06 14:02:56 +0330

    create question

---
+++ src/components/Question/QuestionPage/Create/textMode/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="question-txtMode-navbar">
+    <div class="row">
+      <div class="col flex justify-between">
+        <span>
+          برای ثبت سوال جدید میتوانید تصویر صورت سوال را در این صفحه آپلود کنید یا به صورت مستقیم آن ها را تایپ کنید
+          <q-btn
+            icon-right="isax:info-circle"
+            label="بیشتر بخوانید"
+            flat
+            rounded
+            color="primary"
+          />
+        </span>
+        <q-btn
+          label="تایپ سوال"
+          color="primary"
+          class="change-type"
+          unelevated
+          dense
+        />
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped lang="scss">
+.question-txtMode-navbar{
+  .change-type {
+    width: 144px;
+    height: 40px;
+    border-radius: 10px;
+  }
+}
+</style>
--- src/pages/Admin/Question/CreateText.vue
+++ src/pages/Admin/Question/CreateText.vue
@@ -1,44 +1,22 @@
 <template>
-  <div class="page-container">
-    <div class="row">
-      <div class="col flex justify-between">
-        <span>
-          برای ثبت سوال جدید میتوانید تصویر صورت سوال را در این صفحه آپلود کنید یا به صورت مستقیم آن ها را تایپ کنید
-          <q-btn
-            icon-right="isax:info-circle"
-            label="بیشتر بخوانید"
-            flat
-            rounded
-            color="primary"
-          />
-        </span>
-        <q-btn
-          label="تایپ سوال"
-          color="primary"
-          class="change-type"
-          unelevated
-        />
-      </div>
-    </div>
+  <div class="createQ-text-container">
+    <Navbar/>
   </div>
 </template>
 
 <script>
+import Navbar from 'components/Question/QuestionPage/Create/textMode/Navbar'
 export default {
-  name: 'CreateText'
+  name: 'CreateText',
+  components: { Navbar }
 }
 </script>
 
 <style scoped lang="scss">
-.page-container {
+.createQ-text-container {
   padding: 40px 100px;
   display: flex;
   flex-direction: column;
 
-  .change-type {
-    width: 144px;
-    height: 40px;
-    border-radius: 10px;
-  }
 }
 </style>
