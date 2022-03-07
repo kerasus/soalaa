abd7ad6cde0f6f17a2fbe8f973272bd7a9a81cb8
Nima.mr <nima1380620@yahoo.com>
2022-03-07 11:55:05 +0330

    new questionBank components added!

---
+++ src/components/Question/QuestionBank/components/QuestionBankContent.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-card class="UI-shadow">
+    <q-card-section>
+      سوال 123456789
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'QuestionBankContent'
+}
+</script>
+
+<style scoped>
+
+</style>
