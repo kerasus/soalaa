0fa2227b52f4d90ba863f433998f33271d778adc
hasan <hassan.lookzade@gmail.com>
2022-03-19 15:40:44 +0330

    quasar pagination added

--- src/components/Question/QuestionBank/components/QuestionBankContent.vue
+++ src/components/Question/QuestionBank/components/QuestionBankContent.vue
@@ -108,6 +108,18 @@
       </div>
     </q-card-section>
   </q-card>
+  <div class=" question-bank-pagination q-pa-lg flex flex-center">
+    <q-pagination
+      active-color="primary"
+      outline
+      direction-links
+      v-model="choiceClickedStyle"
+      color="black"
+      :max="10"
+      :max-pages="6"
+      boundary-numbers
+    />
+  </div>
 </template>
 
 <script>
@@ -245,7 +257,7 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
 .question-bank-content {
   padding: 24px;
 
@@ -501,6 +513,15 @@ export default {
         }
       }
     }
+  }
+}
+.question-bank-pagination{
+  .q-btn--actionable{
+    width: 35px;
+    height: 35px;
+    background: #FFFFFF;
+    border-radius: 12px;
+    margin-right: 3px;
   }
 }
 </style>
