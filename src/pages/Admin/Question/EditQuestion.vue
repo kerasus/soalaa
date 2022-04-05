3fa343bcc3c8cf940a00fc4650b4bf71146e0b67
JasGH <yasaman.gonehi@gmail.com>
2022-04-05 13:47:39 +0430

    Edit

---
+++ src/pages/Admin/Question/EditQuestion.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="editQ-text-container">
+    <q-linear-progress
+      v-if="this.question.loading"
+      size="md"
+      indeterminate
+      rounded
+      color="primary"
+    />
+    <div class="relative-position">
+      <component
+        v-if="question.type"
+        :is="getComponent"
+        v-bind="allProps"
+      />
+    </div>
+    <div class="relative-position">
+      <attach-exam
+        :exams="examList"
+        :lessons="subCategoriesList"
+      />
+      <div class="attach-btn row">
+        <question-details class="col-9"/>
+        <btn-box
+          class="col-3"
+          @saveQuestion="setQuestionContents"
+        />
+      </div>
+      <comment-box
+        :statuses="questionStatuses"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-var */
+import { defineAsyncComponent } from 'vue'
+import { Question } from 'src/models/Question'
+import QuestionDetails from 'components/Question/QuestionPage/Create/textMode/QuestionDetails'
+import AdminActionOnQuestion from 'src/mixin/AdminActionOnQuestion'
+import { QuestionType, TypeList } from 'src/models/QuestionType'
+import AttachExam from 'components/Question/QuestionPage/AttachExam'
+import CommentBox from 'components/Question/QuestionPage/CommentBox'
+import BtnBox from 'components/Question/QuestionPage/BtnBox'
+import { ExamList } from 'src/models/Exam'
+import { QuestSubcategoryList } from 'src/models/QuestSubcategory'
+import { QuestionStatusList } from 'src/models/QuestionStatus'
+// import API_ADDRESS from 'src/api/Addresses'
+export default {
+  name: 'EditQuestion',
+  components: {
+    DescriptiveQ: defineAsyncComponent(() => import('components/Question/QuestionPage/Edit/questionTypes/DescriptiveQ/DescriptiveQ')),
+    MultipleChoiceQ: defineAsyncComponent(() => import('components/Question/QuestionPage/Edit/questionTypes/MultipleChoiceQ/MultipleChoiceQ')),
+    MBTIQ: defineAsyncComponent(() => import('components/Question/QuestionPage/Edit/questionTypes/MBTIQ/MBTIQ')),
+    BtnBox,
+    CommentBox,
+    AttachExam,
+    // DynamicComponent,
+    QuestionDetails
+  },
+  mixins: [
+    AdminActionOnQuestion
+  ],
+  props: {},
+  data () {
+    return {
+      questionType: new QuestionType(),
+      componentTabs: new TypeList(),
+      question: new Question(),
+      allProps: {
+        // setContentToQuestion: false
+      },
+      examList: new ExamList(),
+      subCategoriesList: new QuestSubcategoryList(),
+      questionStatuses: new QuestionStatusList()
+    }
+  },
+  created () {
+    // console.log(this.$route, 'this.$route')
+    // console.log(this.$router, 'this.$router')
+  },
+  provide () {
+    return {
+      question: this.question
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.setAllQuestionLoadings()
+      this.loadExamList()
+      this.loadSubcategories()
+      this.getQuestionStatus()
+    })
+  },
+  methods: {
+    chosenComponent () {
+      const cName = 'MultipleChoiceQ'
+      // const cName = this.question.type.componentName
+      if (cName === 'MultipleChoiceQ') {
+        return 'multiple-choice-q'
+      }
+      if (cName === 'DescriptiveQ') {
+        return 'descriptive-q'
+      }
+      if (cName === 'MBTIQ') {
+        return 'm-b-t-i-q'
+      }
+    },
+    setQuestionContents () {
+      // this.allProps.setContentToQuestion = true
+    }
+  },
+  computed: {
+    getComponent () {
+      // updates even if properties inside are updated
+      return this.chosenComponent(this.question.type)
+    }
+  },
+  watch: {
+    question: {
+      handler (newValue, oldValue) {
+        // console.log('question', newValue)
+      },
+      deep: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.editQ-text-container {
+  padding: 40px 100px;
+  display: flex;
+  flex-direction: column;
+}
+</style>
