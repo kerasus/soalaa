488a52f45db8c79219b036d0b4589cd3a23612a6
JasGH <yasaman.gonehi@gmail.com>
2022-04-05 13:47:20 +0430

    moved files

---
+++ src/components/Question/QuestionPage/Show/questionTypes/DescriptiveQ/DescriptiveQ.vue
@@ -0,0 +1,12 @@
+<template>
+  heeeeeeeeeeeeellllllllllooooooooooooooooo DescriptiveQ
+</template>
+
+<script>
+export default {
+  name: 'DescriptiveQ',
+  data () {
+    return {}
+  }
+}
+</script>
