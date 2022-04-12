c098a73351aead02bba53c7bbea3e563eaf6dea2
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-04-12 16:21:53 +0430

    global styles

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 <script>
 import 'src/assets/scss/IranyekanFont.scss'
 import 'src/css/customThem.scss'
+import 'src/css/Theme/theme.scss'
 // import '../public/fonts/iranyekan/css/style.css'
 
 import { mixinSetIcons } from './mixin/Mixins.js'
@@ -20,27 +21,7 @@ export default defineComponent({
 <style>
 #F4F5F6
   :root {
-    --background-1: #fff;
-    --background-2: #f5f5f5;
-    --background-3: red;
-    --surface-1: #fff;
-    --surface-2: #f1f1f1;
-    --surface-3: #f1f1f1;
-    --primary-1: #ffc107;
-    --primary-2: #fbcf4b;
-    --primary-3: #ffc107;
-    --text-1: #000;
-    --text-2: #666;
-    --text-3: #fff;
-    --accent-1: #2196F3;
-    --accent-2: #2196F3;
-    --accent-3: #2196F3;
-    --success-1: #4caf50;
-    --success-2: #4caf50;
-    --success-3: #4caf50;
-    --error-1: #f44336;
-    --error-2: #f44336;
-    --error-3: #f44336;
+
   }
   * {
     /*font-family: 'iranyekan', 'Arial', '-apple-system', 'Helvetica Neue', Helvetica, sans-serif ;*/
