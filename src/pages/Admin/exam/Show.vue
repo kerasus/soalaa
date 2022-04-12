37bf6a221e52cb68f817ea02dfa66c45f02defeb
Mhasanlookzade <hassan.lookzade@gmail.com>
2022-04-12 13:06:46 +0430

    delete btns

--- src/pages/Admin/exam/Show.vue
+++ src/pages/Admin/exam/Show.vue
@@ -1,15 +1,5 @@
 <template>
   <div>
-    <div class="row justify-end q-pr-lg">
-      <q-btn
-        class="q-mx-sm float-right"
-        round
-        dark-percentage
-        color="primary"
-        @click= this.$router.go(-1)
-        icon="isax:arrow-left-2"
-      />
-    </div>
     <entity-show
       v-model:value="inputs"
       title="اطلاعات آزمون"
