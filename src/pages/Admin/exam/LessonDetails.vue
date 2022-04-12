b4910cae547f62e66b85612ef9da45ed6e13959b
mitraKhm <mitra.kh1995@gmail.com>
2022-04-12 17:42:28 +0430

    edit question item  v-3 - lesson detail

--- src/pages/Admin/exam/LessonDetails.vue
+++ src/pages/Admin/exam/LessonDetails.vue
@@ -89,13 +89,14 @@
 </template>
 
 <script>
-import BubbleSheet from 'src/components/OnlineQuiz/Quiz/bubbleSheet/bubbleSheet'
 import Question from 'src/components/QuizEditor/Question'
 import { mixinAuth, mixinQuiz } from 'src/mixin/Mixins'
 import { QuestSubcategoryList } from 'src/models/QuestSubcategory'
 import { QuestionList } from 'src/models/Question'
 import { Exam } from 'src/models/Exam'
 import API_ADDRESS from 'src/api/Addresses'
+import QuestionItem from 'components/Question/QuestionItem/QuestionItem'
+import { copyToClipboard } from 'quasar'
 
 export default {
   name: 'LessonDetails',
