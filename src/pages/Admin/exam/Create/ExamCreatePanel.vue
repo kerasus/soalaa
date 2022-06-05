1bad1f85ca6ad4e3fd49c4bef5aaa8503b97b19e
JasGH <yasaman.gonehi@gmail.com>
2022-06-05 20:53:05 +0430

    base

---
+++ src/pages/Admin/exam/Create/ExamCreatePanel.vue
@@ -0,0 +1,16 @@
+<template>
+<steps/>
+
+</template>
+
+<script>
+import Steps from 'pages/Admin/exam/Create/Steps'
+export default {
+  name: 'ExamCreatePanel',
+  components: { Steps }
+}
+</script>
+
+<style scoped>
+
+</style>
