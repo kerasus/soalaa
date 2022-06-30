9c3c7851a41415e892524b37e7ff09e1d21bf5bf
JasGH <yasaman.gonehi@gmail.com>
2022-06-30 14:21:54 +0430

    MultipleChoiceQuestion bug fix

--- src/pages/Admin/Question/MultipleChoiceQuestion/MultipleChoiceQuestion.vue
+++ src/pages/Admin/Question/MultipleChoiceQuestion/MultipleChoiceQuestion.vue
@@ -180,7 +180,7 @@ export default {
         level: (this.question.level) ? this.question.level : 1,
         reference: this.question.reference,
         years: this.question.years,
-        tags: this.question.tags.map(item => item.id),
+        tags: this.question.tags.list.map(item => item.id),
         majors: this.question.majors,
         sub_category_id: 1,
         recommended_time: 0,
