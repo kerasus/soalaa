ca2f79004fa9cc98263391325ecf076b86e2d546
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-08-15 13:52:33 +0430

    why 3a & 3a features

--- src/pages/User/landing/landing.vue
+++ src/pages/User/landing/landing.vue
@@ -3,14 +3,20 @@
   hi im landing
 </p>
   <landing-header ></landing-header>
+  <why/>
+  <features />
 </template>
 
 <script>
 import landingHeader from 'src/components/landing/header'
+import why from 'src/components/landing/why3A'
+import features from 'src/components/landing/3aFeatures'
 export default {
   name: 'landing',
   components: {
-    landingHeader
+    landingHeader,
+    why,
+    features
   }
 }
 </script>
