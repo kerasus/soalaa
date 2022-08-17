988bc24d4066e3e519f9bddadea12f8e52988f1d
JasGH <yasaman.gonehi@gmail.com>
2022-08-17 11:28:51 +0430

    Permissions middleware

--- src/router/middleware/Permission.js
+++ src/router/middleware/Permission.js
@@ -1,33 +1,32 @@
 class Permissions {
   static hasAllOfThese (permissionArray) {
-    return ({ next, store, to }) => {
-      const user = store.getters['Auth/user']
-      const notIncludedPermissions = permissionArray.find(permission => !user.hasPermission(permission))
-      console.log('notIncludedPermissions', notIncludedPermissions)
-      if (notIncludedPermissions) {
-        return next({ name: 'dashboard' })
-      }
-      return next()
-    }
+    return this.hasPermission(permissionArray, 'hasAll')
   }
 
   static hasOneOfThese (permissionArray) {
-    return ({ next, store, to }) => {
-      const user = store.getters['Auth/user']
-      const includedPermissions = permissionArray.find(permission => user.hasPermission(permission))
-      console.log('includedPermissions', includedPermissions)
-      if (!includedPermissions) {
-        return next({ name: 'dashboard' })
-      }
-
-      return next()
-    }
+    return this.hasPermission(permissionArray, 'hasOne')
   }
 
-  static hasPermission (permission) {
+  static hasPermission (permission, mode) {
     return ({ next, store, to }) => {
       const user = store.getters['Auth/user']
-      if (!user.hasPermission(permission)) {
+      let StopRouting = false
+      if (typeof permission === 'string') {
+        if (!user.hasPermission(permission)) {
+          StopRouting = true
+        }
+      } else if (mode === 'hasOne') {
+        const includedPermissions = permission.find(permission => user.hasPermission(permission))
+        if (!includedPermissions) {
+          StopRouting = true
+        }
+      } else if (mode === 'hasAll') {
+        const notIncludedPermissions = permission.find(permission => !user.hasPermission(permission))
+        if (notIncludedPermissions) {
+          StopRouting = true
+        }
+      }
+      if (StopRouting) {
         return next({ name: 'dashboard' })
       }
 
