65572859fb73a312dfbc2d897f46cef65c45568b
Archer-Thane <bahrami.ehssan@gmail.com>
2022-08-17 13:05:00 +0430

    adding wireframe for cart items

---
+++ src/pages/cart.vue
@@ -0,0 +1,67 @@
+<template>
+  <page-builder
+    :sections="sections"
+    :options="options"
+    :containerFullHeight="calculateHeightStyle"
+  ></page-builder>
+</template>
+
+<script>
+import pageBuilder from 'src/components/PageBuilder/PageBuilder'
+
+export default {
+  name: 'cart',
+  components: { pageBuilder },
+  data() {
+    return {
+      sections: [
+        {
+          data: {
+            rows: [
+              {
+                cols: [
+                  {
+                    widgets: [
+                      {
+                        name: 'cart-view'
+                      }
+                    ],
+                    options: {
+                      className: 'col-md-9 q-pr-md col-sm-12'
+                    }
+                  },
+                  {
+                    widgets: [
+                      {
+                        name: 'test-component2-widget'
+                      }
+                    ],
+                    options: {
+                      className: 'col-md-3 q-pl-md col-sm-12'
+                    }
+                  }
+                ],
+                options: {
+                  boxed: true,
+                  style: {
+                    marginTop: '30px'
+                  }
+                }
+              }
+            ]
+          },
+          options: {}
+        }
+      ],
+      options: []
+    }
+  },
+  computed: {
+    calculateHeightStyle() {
+      return this.$store.getters['AppLayout/calculateContainerFullHeight']
+    }
+  }
+}
+</script>
+
+<style scoped></style>
