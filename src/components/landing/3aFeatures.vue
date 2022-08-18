72ec51864fb4a09c881c42be2707969e209c5e35
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-08-18 12:39:16 +0430

    bug fixed

--- src/components/landing/3aFeatures.vue
+++ src/components/landing/3aFeatures.vue
@@ -4,27 +4,48 @@
 
     <div class="landing-3a-features">
       <div class="landing-3a-feature">
-        <q-img :width="imageWidth"  src="https://nodes.alaatv.com/upload/landing/3a/7.png"/>
+        <q-img
+          src="https://nodes.alaatv.com/upload/landing/3a/7.png"
+          :width="imageWidth[0]"
+          class="feature-image"
+        />
         <p class="feature-title">سه‌آ در سه شنبه ها</p>
       </div>
 
       <div class="landing-3a-feature">
-        <q-img :width="imageWidth"  src="https://nodes.alaatv.com/upload/landing/3a/8.png"/>
+        <q-img
+          src="https://nodes.alaatv.com/upload/landing/3a/8.png"
+          :width="imageWidth[1]"
+          class="feature-image"
+        />
         <p class="feature-title">راحت در موبایل</p>
       </div>
 
       <div class="landing-3a-feature">
-        <q-img :width="imageWidth"  src="https://nodes.alaatv.com/upload/landing/3a/9.png"/>
+        <q-img
+          src="https://nodes.alaatv.com/upload/landing/3a/9.png"
+          :width="imageWidth[2]"
+          class="feature-image"
+        />
         <p class="feature-title">کارنامه آنی</p>
       </div>
 
       <div class="landing-3a-feature">
-        <q-img :width="imageWidth"  src="https://nodes.alaatv.com/upload/landing/3a/10.png"/>
+        <q-img
+          src="https://nodes.alaatv.com/upload/landing/3a/10.png"
+          :width="imageWidth[3]"
+          class="feature-image"
+        />
         <p class="feature-title">تست‌ها مطابق کنکور</p>
       </div>
 
       <div class="landing-3a-feature">
-        <q-img :width="imageWidth"  src="https://nodes.alaatv.com/upload/landing/3a/12.png"/>
+        <q-img
+          src="https://nodes.alaatv.com/upload/landing/3a/12.png"
+          :width="imageWidth[4]"
+
+          class="feature-image"
+        />
         <p class="feature-title">پاسخنامه ویدئویی</p>
       </div>
     </div>
@@ -37,33 +58,32 @@ export default {
   data () {
     return {
       imageWidthList: [
-        { xl: '240px', lg: '192px', md: '242px', sm: '212px', xs: '272px' },
-        { xl: '191px', lg: '154px', md: '192px', sm: '169px', xs: '220px' },
-        { xl: '191px', lg: '154px', md: '168px', sm: '169px', xs: '210px' },
-        { xl: '140px', lg: '108px', md: '131px', sm: '126px', xs: '146px' },
-        { xl: '121px', lg: '91px', md: '128px', sm: '104px', xs: '130px' }
+        { lg: '240px', md: '170px', sm: '130px', xs: '110px' },
+        { lg: '240px', md: '170px', sm: '130px', xs: '110px' },
+        { lg: '240px', md: '170px', sm: '131px', xs: '112px' },
+        { lg: '212px', md: '168px', sm: '126px', xs: '104px' },
+        { lg: '230px', md: '180px', sm: '126px', xs: '110px' }
       ],
       currentImagesWidthList: []
     }
   },
+
   computed: {
     imageWidth () {
-      return this.setImageWidth()
+      return this.getImageWidth()
     },
 
     windowSize () {
       return this.$store.getters['AppLayout/windowSize']
     }
   },
+
   methods: {
-    setImageWidth () {
+    getImageWidth () {
+      console.log('feature')
       this.currentImagesWidthList = []
 
       if (this.windowSize.x > 1439) {
-        this.imageWidthList.forEach(item => {
-          this.currentImagesWidthList.push(item.xl)
-        })
-      } else if (this.windowSize.x > 1023) {
         this.imageWidthList.forEach(item => {
           this.currentImagesWidthList.push(item.lg)
         })
@@ -79,9 +99,8 @@ export default {
         this.imageWidthList.forEach(item => {
           this.currentImagesWidthList.push(item.xs)
         })
-
-        return this.currentImagesWidthList
       }
+      return this.currentImagesWidthList
     }
   }
 }
@@ -133,13 +152,15 @@ export default {
     .landing-3a-feature {
       display: flex;
       flex-direction: column;
+      justify-content: space-between;
       align-items: center;
       width: 350px;
-      height: 286px;
+      height: 277px;
+      margin-bottom: 20px;
 
       @media only screen and (max-width: 1439px) {
         width: 290px;
-        height: 230px;
+        height: 210px;
       }
 
       @media only screen and (max-width: 1023px) {
@@ -149,11 +170,16 @@ export default {
       @media only screen and (max-width: 768px) {
         width: 150px;
         height: 167px;
+        margin-bottom: 10px;
+        margin-left: 10px;
+        margin-right: 10px;
       }
 
       @media only screen and (max-width: 600px) {
         width: 144px;
-        height: 164px;
+        height: 138px;
+        margin-left: 0;
+        margin-right: 0;
       }
 
       .feature-title {
@@ -162,6 +188,7 @@ export default {
         font-size: 20px;
         line-height: 32px;
         color: #1E1E1E;
+        margin-bottom: 0;
 
         @media only screen and (max-width: 1439px) {
           font-size: 18px;
@@ -175,6 +202,15 @@ export default {
           font-size: 12px;
         }
       }
+
+      .feature-image {
+        margin-bottom: 30px;
+
+        @media only screen and (max-width: 1439px) {
+          margin-bottom: 10px;
+        }
+      }
+
     }
   }
 
