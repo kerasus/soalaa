e65b54b2c7e3c62e1a877858e73f25342604bc48
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-08-21 10:58:05 +0430

    logic of cart see also done

--- src/components/Widgets/TestComponent2Widget/TestComponent2Widget.vue
+++ src/components/Widgets/TestComponent2Widget/TestComponent2Widget.vue
@@ -4,16 +4,13 @@
       <h1>hi</h1>
     </div>
     <div class="col-md-4 col-12">
-      <cart-invoice/>
     </div>
   </div>
 </template>
 
 <script>
-import CartInvoice from 'src/components/Widgets/CartInvoice/CartInvoice.vue'
 export default {
-  name: 'Test-component-2widget',
-  components: { CartInvoice }
+
 }
 </script>
 
