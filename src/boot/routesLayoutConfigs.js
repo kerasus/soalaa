6598c304958bfe2cc5986a71506947c578f998da
JasGH <yasaman.gonehi@gmail.com>
2022-08-22 10:49:57 +0430

    routesLayoutConfigs added

--- src/boot/routesLayoutConfigs.js
+++ src/boot/routesLayoutConfigs.js
@@ -1,9 +1,23 @@
 import { boot } from 'quasar/wrappers'
 
+function setLayoutConfig (routes, toName, store) {
+  for (const routeIndex in routes.children) {
+    const route = routes.children[routeIndex]
+    if (route.layoutConfig) {
+      store.dispatch('AppLayout/updateTemplateHeaderType', route.layoutConfig)
+    }
+    if (route.name === toName) {
+      return
+    }
+
+    if (route.children && route.children.length > 0) {
+      setLayoutConfig(route, toName, store)
+    }
+  }
+}
+
 export default boot(({ router, store }) => {
-  router.beforeEach((to, from, next) => {
-    store.dispatch('AppLayout/updateTemplateHeaderType', to.layoutConfig)
-    store.commit('AppLayout/updateTemplateHeaderType', to.name)
-    // return
+  router.beforeEach((to) => {
+    setLayoutConfig({ children: router.options.routes }, to.name, store)
   })
 })
