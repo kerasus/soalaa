bd5e8c6eb0a49040c412fb3c1c4b27c1ea28146c
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-08-22 09:11:00 +0430

    cart actions fixed

--- src/store/Cart/actions.js
+++ src/store/Cart/actions.js
@@ -21,13 +21,19 @@ export function addToCart (context, product) {
         })
     } else {
       cart.addToCart(product)
-      CookieCart.addToCartInCookie(cart)
       return resolve(true)
     }
   })
 }
 
 export function reviewCart (context, product) {
+  const isUserLogin = !!this.getters['Auth/isUserLogin']
+  const currentCart = context.getters.cart
+
+  if (!isUserLogin) {
+    CookieCart.addToCartInCookie(currentCart)
+  }
+
   return new Promise((resolve, reject) => {
     axios
       .get(API_ADDRESS.cart.review)
@@ -69,7 +75,7 @@ export function removeItemFromCart (context, productId) {
   return new Promise((resolve, reject) => {
     if (isUserLogin) {
       axios
-        .delete(API_ADDRESS.cart.orderproduct + '/' + productId)
+        .delete(API_ADDRESS.cart.orderproduct(productId))
         .then((response) => {
           return resolve(response)
         })
@@ -79,9 +85,8 @@ export function removeItemFromCart (context, productId) {
     } else {
       const cart = context.getters.cart
 
-      cart.cartItems.list = cart.cartItems.list.filter((item) => {
-        return item.id !== productId
-      })
+      cart.removeItem(productId)
+      context.commit('updateCart', cart)
 
       CookieCart.removeCartItemFromCookieCart(productId)
       return resolve(true)
