1b5340f335dcba26d966e3f9c7dddc3205138482
JasGH <yasaman.gonehi@gmail.com>
2022-08-23 16:29:41 +0430

    QuestionAuthors

--- src/pages/Admin/QuestionAuthors.vue
+++ src/pages/Admin/QuestionAuthors.vue
@@ -124,16 +124,16 @@ export default {
       defaultInputs: [
         { type: 'hidden', name: 'id', label: 'شناسه', responseKey: 'data.id', col: 'col-md-1', placeholder: ' ', filled: true },
         { type: 'hidden', name: 'type', label: 'نوع', responseKey: 'data.type', col: 'col-md-1', placeholder: ' ', filled: true },
-        { type: 'input', name: 'value', label: 'مرجع', responseKey: 'data.value', col: 'col-md-3', placeholder: ' ', filled: true }
+        { type: 'input', name: 'value', label: 'عنوان', responseKey: 'data.value', col: 'col-md-3', placeholder: ' ', filled: true }
       ],
       createInputs: [
         { type: 'hidden', name: 'type', value: 'reference_type', label: '', col: 'col-12' },
-        { type: 'input', name: 'value', label: 'مرجع', col: 'col-md-3', placeholder: ' ', filled: true }
+        { type: 'input', name: 'value', label: 'عنوان مرجع', col: 'col-md-3', placeholder: ' ', filled: true }
       ],
       editInputs: [],
       showInputs: [],
       indexInputs: [
-        { type: 'input', name: 'value', label: 'مرجع', responseKey: 'data.value', col: 'col-md-3', placeholder: ' ', filled: true }
+        { type: 'input', name: 'value', label: 'عنوان', responseKey: 'data.value', col: 'col-md-3', placeholder: ' ', filled: true }
       ]
     }
   },
