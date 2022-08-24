44ab764cb3cee4759aa994de6d32a9312159a78f
JasGH <yasaman.gonehi@gmail.com>
2022-08-24 12:41:20 +0430

    ExamPage

--- src/pages/Admin/exam/index.vue
+++ src/pages/Admin/exam/index.vue
@@ -179,8 +179,8 @@ export default {
       inputs: [
         { type: 'input', name: 'title', label: 'عنوان', responseKey: 'data.title', col: 'col-md-3', placeholder: ' ', filled: true },
         // { type: 'input', name: 'type', label: 'نوع', responseKey: 'data.type', col: 'col-md-3', placeholder: ' ', filled: true },
-        { type: 'date', name: 'updated_at', label: '', responseKey: 'data.updated_at', col: 'col-md-3', placeholder: 'تاریخ شروع از' },
-        { type: 'date', name: 'created_at', label: '', responseKey: 'data.created_at', col: 'col-md-3', placeholder: 'تاریخ شروع تا' }
+        { type: 'date', name: 'started_from', label: '', col: 'col-md-3', placeholder: 'تاریخ شروع از' },
+        { type: 'date', name: 'started_till', label: '', col: 'col-md-3', placeholder: 'تاریخ شروع تا' }
       ]
     }
   },
