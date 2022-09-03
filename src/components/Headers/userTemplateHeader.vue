4539453329c81d39064d4f059a18da992fb9e65b
JasGH <yasaman.gonehi@gmail.com>
2022-09-03 11:42:46 +0430

    UserTemplateHeader Update

--- src/components/Headers/userTemplateHeader.vue
+++ src/components/Headers/userTemplateHeader.vue
@@ -1,95 +1,86 @@
 <template>
-  <div class="flex justify-center user-app-bar-container">
+  <div class="user-app-bar-container">
     <div class="app-bar">
-      <div class="row">
+      <div class="user-header-section">
         <!--        -----------------------------------------------------Logo Section--------------------------------------------   -->
-        <div class="col-3 flex logo">
-          <div class="logo-pic">
-            <q-img src="https://nodes.alaatv.com/aaa/landing/Soalaa/Logo/logo.png"
-                   width="71px"
-                   height="71px" />
+        <div class="user-logo-section">
+          <div class="hamburger">
+            <q-btn
+              flat
+              @click="toggleLeftDrawer"
+            >
+              <svg width="24"
+                   height="24"
+                   viewBox="0 0 24 24"
+                   fill="none"
+                   xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 7H21"
+                      stroke="#6D708B"
+                      stroke-width="1.5"
+                      stroke-linecap="round" />
+                <path d="M3 12H21"
+                      stroke="#6D708B"
+                      stroke-width="1.5"
+                      stroke-linecap="round" />
+                <path d="M3 17H21"
+                      stroke="#6D708B"
+                      stroke-width="1.5"
+                      stroke-linecap="round" />
+              </svg>
+            </q-btn>
           </div>
-          <div>
-            <q-btn flat
-                   icon="mdi-more"
-                   @click="toggleLeftDrawer" />
+          <div class="logo-pic">
+            <q-img
+              class="logo-pic-img"
+              src="https://nodes.alaatv.com/aaa/landing/Soalaa/Logo/logo.png"
+            />
           </div>
         </div>
         <!--        -----------------------------------------------------Tabs Section--------------------------------------------   -->
-        <div class="col-6 flex justify-center">
+        <div class="user-tab-section">
           <q-list class="flex tabs-list">
             <q-item
+              v-for="(item, index) in headerItems"
+              :key="index"
               v-ripple
               clickable
-              :active="selected === 'exams'"
+              :active="selected === item.selected"
               active-class="active-item"
-              @click="selected = 'exams'">
+              @click="selected = item.selected">
               <q-item-section class="tab-title">
-                آزمون ها
-              </q-item-section>
-            </q-item>
-            <q-item
-              v-ripple
-              clickable
-              :active="selected === 'questionBank'"
-              active-class="active-item"
-              @click="selected = 'questionBank'">
-              <q-item-section class="tab-title">
-                بانک سوال
-              </q-item-section>
-            </q-item>
-            <q-item
-              v-ripple
-              clickable
-              :active="selected === 'soalaMag'"
-              active-class="active-item"
-              @click="selected = 'soalaMag'">
-              <q-item-section class="tab-title">
-                سوالامگ
-              </q-item-section>
-            </q-item>
-            <q-item
-              v-ripple
-              clickable
-              :active="selected === 'askedQuestions'"
-              active-class="active-item"
-              @click="selected = 'askedQuestions'">
-              <q-item-section class="tab-title">
-                سوالات متداول
-              </q-item-section>
-            </q-item>
-            <q-item
-              v-ripple
-              clickable
-              :active="selected === 'contactUs'"
-              active-class="active-item"
-              @click="selected = 'contactUs'">
-              <q-item-section class="tab-title">
-                تماس با ما
+                {{ item.title }}
               </q-item-section>
             </q-item>
           </q-list>
         </div>
         <!--        -----------------------------------------------------Actions Section--------------------------------------------   -->
-        <div class="col-3 user-action">
+        <div class="user-action">
           <div>
-            <q-btn icon="isax:notification"
-                   unelevated
-                   rounded
-                   size="12px"
-                   class="action-btn" />
-            <q-btn icon="isax:shopping-bag"
-                   unelevated
-                   rounded
-                   size="12px"
-                   class="action-btn" />
+            <q-btn
+              icon="isax:notification"
+              unelevated
+              rounded
+              size="12px"
+              class="action-btn"
+            />
+            <q-btn
+              icon="isax:shopping-bag"
+              unelevated
+              rounded
+              size="12px"
+              class="action-btn"
+            />
           </div>
-          <q-btn flat
-                 class="btn-user-profile">
-            <q-img class="user-photo"
-                   :src="'https://cdn.quasar.dev/img/avatar2.jpg'"
-                   width="48px"
-                   height="48px" />
+          <q-btn
+            flat
+            class="btn-user-profile"
+          >
+            <q-img
+              class="user-photo"
+              :src="user.photo"
+              width="48px"
+              height="48px"
+            />
           </q-btn>
         </div>
       </div>
@@ -100,12 +91,35 @@
 <script>
 
 import { mapGetters, mapMutations } from 'vuex'
+import { User } from 'src/models/User'
 
 export default {
   name: 'UserTemplateHeader',
   data () {
     return {
-      selected: 'exams'
+      selected: 'exams',
+      headerItems: [
+        {
+          selected: 'exams',
+          title: 'آزمون ها'
+        },
+        {
+          selected: 'questionBank',
+          title: 'بانک سوال'
+        },
+        {
+          selected: 'soalaMag',
+          title: 'سوالامگ'
+        },
+        {
+          selected: 'askedQuestions',
+          title: 'سوالات متداول'
+        },
+        {
+          selected: 'contactUs',
+          title: 'تماس با ما'
+        }
+      ]
     }
   },
   computed: {
@@ -114,7 +128,13 @@ export default {
     ]),
     ...mapGetters('AppLayout', [
       'layoutLeftDrawerVisible'
-    ])
+    ]),
+    user () {
+      if (this.$store.getters['Auth/user']) {
+        return this.$store.getters['Auth/user']
+      }
+      return new User()
+    }
   },
   methods: {
     ...mapMutations('AppLayout', [
@@ -131,60 +151,137 @@ export default {
 .user-app-bar-container {
   background-color: #fff;
   height: 72px;
-  align-items: center;
+  width: 100%;
+  @media screen and (max-width: 1023px) {
+    height: 64px;
+  }
+  @media screen and (max-width: 599px) {
+    background-color: #F4F6F9;
+  }
   .app-bar {
     max-width: 1360px;
-    min-width: 1360px;
-    .logo {
-      align-items: center;
-      .logo-pic {
-        cursor: pointer;
-        height: 100%;
-        display: flex;
+    //min-width: 1360px;
+    margin: auto;
+    height: 72px;
+    @media screen and (max-width: 1919px) {
+      width: 100%;
+      padding-left: 40px;
+      padding-right: 40px;
+    }
+    @media screen and (max-width: 1439px) {
+      padding-left: 35px;
+      padding-right: 35px;
+    }
+    @media screen and (max-width: 1023px) {
+      padding-left: 20px;
+      padding-right: 20px;
+      height: 64px;
+    }
+    .user-header-section {
+      display: grid;
+      grid-template-columns: 132px auto 156px;
+      height: 100%;
+      @media screen and (max-width: 1023px) {
+        grid-template-columns: auto auto;
+      }
+      @media screen and (max-width: 599px) {
+        grid-template-columns: 1fr;
+      }
+      .user-logo-section {
         align-items: center;
-      }
-    }
-    .tabs-list {
-      height: 72px;
-      .tab-title {
-        //font-family: 'Yekan Bakh FaNum';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 16px;
-        line-height: 25px;
-      }
-    }
-    .active-item {
-      color: #9690E4;
-    }
-    .user-action {
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
-      .btn-user-profile {
-        margin-left: 18px;
-        width: 48px;
-        height: 48px;
-        border-radius: 16px;
-        :deep(.q-btn__content) {
-          margin: 0;
-          .user-photo {
-            img {
-              border: 2px solid #FFB74D;
-              border-radius: 16px;
-              max-width: 100%;
-              width: 100%;
+        display: flex;
+        @media screen and (max-width: 1023px) {
+          justify-self: start;
+        }
+        @media screen and (max-width: 599px) {
+          justify-self: center;
+          justify-content: space-between;
+          width: 100%;
+        }
+        .logo-pic {
+          cursor: pointer;
+          display: flex;
+          height: 72px;
+          align-items: center;
+          @media screen and (max-width: 1023px) {
+            height: 64px;
+          }
+          .logo-pic-img {
+            height: 72px;
+            width: 72px;
+            @media screen and (max-width: 1023px) {
+              height: 48px;
+              width: 48px;
             }
+          }
+          //img {
+          //
+          //}
+        }
+        .hamburger {
+          display: none;
+          @media screen and (max-width: 1023px) {
+            display: block;
+            margin-right: 20px;
           }
         }
       }
-      .action-btn {
-        margin: 4px;
+      .user-tab-section {
+        height: 72px;
+        .tabs-list {
+          height: 72px;
+          .tab-title {
+            font-style: normal;
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 25px;
+          }
+        }
+        @media screen and (max-width: 1023px) {
+          height: 64px;
+          display: none;
+        }
       }
-      .fit-profile-img {
-        width: 48px;
-        height: 48px;
-        border-radius: 16px;
+      .active-item {
+        color: #9690E4;
+      }
+      .user-action {
+        @media screen and (max-width: 1023px) {
+          justify-self: end;
+          height: 64px;
+        }
+        @media screen and (max-width: 599px) {
+          display: none;
+        }
+        display: flex;
+        //justify-content: flex-end;
+        align-items: center;
+        height: 72px;
+        .btn-user-profile {
+          margin-left: 18px;
+          width: 48px;
+          height: 48px;
+          border-radius: 16px;
+          :deep(.q-btn__content) {
+            margin: 0;
+            .user-photo {
+              img {
+                border: 2px solid #FFB74D;
+                border-radius: 16px;
+                max-width: 100%;
+                width: 100%;
+              }
+            }
+          }
+        }
+        .action-btn {
+          margin: 4px;
+        }
+        .fit-profile-img {
+          width: 48px;
+          height: 48px;
+          border-radius: 16px;
+        }
       }
     }
   }
