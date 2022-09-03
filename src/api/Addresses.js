b243a050a35a32872c9de25dbf1e804f6b6aad7e
JasGH <yasaman.gonehi@gmail.com>
2022-09-03 12:57:53 +0430

    my orders

--- src/api/Addresses.js
+++ src/api/Addresses.js
@@ -25,8 +25,7 @@ const API_ADDRESS = {
     formData: authServer + '/megaroute/getUserFormData',
     show_user: authServer + '/getUserFor3a',
     getOrderList (id) {
-      // return authServer + '/user/' + id + '/orders?seller=2'
-      return authServer + '/user/' + id + '/orders?seller=1'
+      return authServer + '/user/' + id + '/orders?seller=2'
     }
   },
   set: {
