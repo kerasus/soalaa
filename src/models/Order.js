b587fd1c605aba36e1985d5f4a62c2d4255419d6
JasGH <yasaman.gonehi@gmail.com>
2022-09-03 13:54:49 +0430

    Order Model

--- src/models/Order.js
+++ src/models/Order.js
@@ -3,7 +3,6 @@ import { User } from './User'
 import { Coupon } from './Coupon'
 // import {OrderProductList} from "src/models/OrderProduct";
 import { OrderItemCollection } from './OrderItem'
-
 class Order extends Model {
   constructor (data) {
     super(data, [
@@ -18,6 +17,14 @@ class Order extends Model {
         key: 'coupon_info',
         relatedModel: Coupon
       },
+      // {
+      //   key: 'order_product',
+      //   relatedModel: OrderItemCollection
+      // },
+      {
+        key: 'orderproduct',
+        relatedModel: OrderItemCollection
+      },
       {
         key: 'orderstatus',
         default: {}
@@ -26,15 +33,6 @@ class Order extends Model {
         key: 'paymentstatus',
         default: {}
       },
-      {
-        key: 'orderproduct',
-        default: OrderItemCollection
-      },
-
-      // {
-      //   key: 'order_product',
-      //   relatedModel: OrderProductList
-      // },
       { key: 'successful_transactions' },
       { key: 'pending_transactions' },
       { key: 'unpaid_transaction' },
@@ -46,63 +44,6 @@ class Order extends Model {
       { key: 'created_at' },
       { key: 'completed_at' }
     ])
-
-    const that = this
-    this.apiResource = {
-      fields: [
-        { key: 'id' },
-        { key: 'discount' },
-        { key: 'customer_description' },
-        { key: 'price' },
-        { key: 'paid_price' },
-        { key: 'refund_price' },
-        { key: 'debt' },
-        {
-          key: 'orderstatus',
-          default: {}
-        },
-        {
-          key: 'paymentstatus',
-          default: {}
-        },
-        // {
-        //   key: 'orderproducts',
-        //   default: OrderProductList
-        // },
-        //
-        {
-          key: 'order_product',
-          // relatedModel: OrderProductList
-          value: function () {
-            return that.orderproducts.list
-          }
-        },
-        {
-          key: 'coupon_info',
-          value: function () {
-            return that.coupon_info
-          }
-        },
-        { key: 'successful_transactions' },
-        { key: 'pending_transactions' },
-        { key: 'unpaid_transaction' },
-        { key: 'posting_info' },
-        {
-          key: 'user',
-          value: function () {
-            return that.user
-          }
-        },
-        { key: 'created_at' },
-        { key: 'completed_at' },
-        {
-          key: 'choices',
-          value: function () {
-            return that.choices.list
-          }
-        }
-      ]
-    }
   }
 }
 
