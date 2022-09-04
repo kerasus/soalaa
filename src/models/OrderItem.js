bf9f37278896b1d4a8182cff144fa793faf16296
JasGH <yasaman.gonehi@gmail.com>
2022-09-04 18:02:45 +0430

    models updated

--- src/models/OrderItem.js
+++ src/models/OrderItem.js
@@ -1,30 +1,35 @@
 import { Model, Collection } from 'js-abstract-model'
 import { Product } from './Product'
-import Price from './Price'
+// import Price from './Price'
+import { OrderProductList } from 'src/models/OrderProduct'
 
 class OrderItem extends Model {
   constructor(data) {
     super(data, [
-      { key: 'id' },
+      // { key: 'id' },
+      // {
+      //   key: 'quantity',
+      //   default: 1
+      // },
+      // { key: 'type' },
+      // {
+      //   key: 'product',
+      //   relatedModel: Product
+      // },
       {
-        key: 'quantity',
-        default: 1
-      },
-      { key: 'type' },
-      {
-        key: 'product',
-        relatedModel: Product
+        key: 'order_product',
+        relatedModel: OrderProductList
       },
       {
         key: 'grand',
         relatedModel: Product
-      },
-      {
-        key: 'price',
-        relatedModel: Price
-      },
-      { key: 'photo' },
-      { key: 'extra_attributes' }
+      }
+      // {
+      //   key: 'price',
+      //   relatedModel: Price
+      // },
+      // { key: 'photo' },
+      // { key: 'extra_attributes' }
     ])
   }
 }
