37f3527cb2e257a23e8f0f594e447519482adeef
JasGH <yasaman.gonehi@gmail.com>
2022-09-06 11:19:27 +0430

    board-container stable

--- src/components/Widgets/Dashboard/SideComponent/SubscriptionStatus.vue
+++ src/components/Widgets/Dashboard/SideComponent/SubscriptionStatus.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>وضعیت اشتراک</div>
+  <div class="subscription-status">
+
+  </div>
 </template>
 
 <script>
@@ -8,6 +10,20 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.subscription-status {
+  background: #FFFFFF;
+  box-shadow: -2px -4px 10px rgba(255, 255, 255, 0.6), 2px 4px 10px rgba(112, 108, 162, 0.05) #{"/* rtl:ignore */"};
+  border-radius: 16px;
+  height: 489px;
+  @media screen and (max-width: 1439px) {
+    height: 410px;
+  }
+  @media screen and (max-width: 1023px) {
+    height: 443px;
+  }
+  @media screen and (max-width: 1023px) {
+    height: 312px;
+  }
+}
 </style>
