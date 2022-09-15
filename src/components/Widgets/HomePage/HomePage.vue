a61ca48b59935972b01b4a8b01854215076e6265
mitraKhm <mitra.kh1995@gmail.com>
2022-09-15 15:20:19 +0430

    desktop style done

--- src/components/Widgets/HomePage/HomePage.vue
+++ src/components/Widgets/HomePage/HomePage.vue
@@ -1,5 +1,4 @@
 <template>
-  <div class="bg-pink-1 text-bold"> hi home page </div>
   <q-skeleton v-if="loading"
               class="full-width slider-loading"
               height="520px" />
@@ -36,12 +35,13 @@
       outside-arrows
       mobile-arrows
       indicator-color="transparent"
-      active-class=""
+      active-class="active-tab"
       class=""
     >
       <q-tab v-for="(tab, index) in pageData.tabs"
              :key="index"
              :name="index"
+             icon="img:https://nodes.alaatv.com/aaa/landing/Soalaa/HomePage/attr/2_on.png"
              class="tab-style"
              :label="tab.key"
       />
@@ -53,7 +53,7 @@
                    :key="index"
                    class="q-pa-none flex"
                    :name="index">
-        <div class="content-box">
+        <div class="content">
           <div class="title"> {{tabData.key}}</div>
           <div class="description">{{tabData.value}}</div>
           <div v-if="tabData.link"
@@ -63,6 +63,7 @@
               :to="tabData.link"
               style="color: #8075DC"
               class="btn"
+              padding="9px 17px"
               icon-right="west"
               label="اطلاعات بیشتر" />
           </div>
@@ -94,6 +95,18 @@
                 name="circle" />
         {{item}}
       </div>
+      <div v-if="pageData.bank_soala.link"
+           class="more-details text-right">
+        <q-btn
+          unelevated
+          color="primary"
+          :to="pageData['3a_azmon'].link"
+          style="color: #8075DC"
+          class="btn q-ma-none"
+          padding="9px 17px 9px 19px"
+          icon-right="west"
+          label="اطلاعات بیشتر" />
+      </div>
     </div>
     <div class="img-box">
       <q-img
@@ -101,11 +114,53 @@
         :srs="pageData.bank_soala.image" />
     </div>
   </div>
-  <div class="sub-mit-box flex justify-between">
+  <div class="sub-mit-box flex justify-between items-center">
     <div class="info">با ورود و یا ثبت نام در سوالا میتوانید محتوای شخصی سازی شده و مربوط به خود را دنبال کنید</div>
     <div class="action-box">
-
+      <q-btn
+        unelevated
+        class="btn-style"
+        label="ورود"></q-btn>
+      <q-btn unelevated
+             color="primary"
+             class="btn-style"
+             label="ثبت نام"></q-btn>
     </div>
+  </div>
+  <q-skeleton v-if="loading"
+              class="full-width slider-loading"
+              height="520px" />
+  <div v-else
+       class="sea-exams flex">
+    <div class="content">
+      <div class="title">آزمون‌های سه‌آ</div>
+      <div class="sub-title">{{pageData['3a_azmon'].title}}</div>
+      <div v-for="(item, index) in pageData['3a_azmon'].value"
+           :key="index"
+           class="item">
+        <q-icon size="7px"
+                class="q-mr-sm"
+                name="circle" />{{item}}
+      </div>
+      <div v-if="pageData['3a_azmon'].link"
+           class="more-details text-right">
+        <q-btn
+          unelevated
+          color="primary"
+          :to="pageData['3a_azmon'].link"
+          style="color: #8075DC"
+          class="btn q-ma-none"
+          padding="9px 17px 9px 19px"
+          icon-right="west"
+          label="اطلاعات بیشتر" />
+      </div>
+    </div>
+    <div class="img-box">
+      <q-img
+        class="img"
+        :srs="pageData['3a_azmon'].image" />
+    </div>
+
   </div>
 
 </template>
@@ -164,6 +219,23 @@ export default {
   border-radius: 30px;
   height: 520px;
   margin: 40px 0;
+  &:deep(.q-carousel__navigation-inner){
+    .q-btn{
+      margin: 0;
+      padding: 0;
+      .q-spinner{
+        font-size: 10px;
+      }
+
+    }
+  }
+  &:deep(.q-carousel__navigation-icon--active){
+    .q-btn{
+      .q-spinner{
+        font-size: 15px;
+      }
+    }
+  }
   @media screen and (max-width: 1439px){
     height: 364px;
   }
@@ -172,6 +244,22 @@ export default {
   }
   @media screen and (max-width: 599px){
     height: 360px;
+  }
+}
+.content{
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: min-content;
+  .more-detail{
+    align-self: flex-end;
+    .btn{
+      font-style: normal;
+      font-weight: 600;
+      font-size: 14px;
+      line-height: 22px;
+      text-align: center;
+      letter-spacing: -0.03em;
+    }
   }
 }
 .slider-loading{
@@ -185,7 +273,7 @@ export default {
   line-height: 31px;
   letter-spacing: -0.03em;
   color: #6D708B;
-  margin:20px 0 16px 0;
+  padding:20px 0 16px 0;
 }
 .tab-title{
   font-style: normal;
@@ -199,6 +287,13 @@ export default {
 }
 .tab-container{
   margin-bottom: 80px;
+  .active-tab{
+    &:deep(.q-tab__content){
+        .q-tab__label{
+          color: #9690E4;
+        }
+    }
+  }
   .active-container{
     color: #9690E4;
   }
@@ -214,13 +309,19 @@ export default {
       letter-spacing: -0.03em;
       color: #8A8CA6;
     }
+    &:deep(.q-tab__content){
+        .q-icon{
+          width: 28px;
+          height: 28px;
+        }
+    }
 
   }
   .tab-content{
     background: rgba(228, 232, 239, .6);
     border-radius: 20px;
     padding: 40px 80px;
-    .content-box{
+    .content{
       margin-right: 40px;
       .title{
         font-weight: 700;
@@ -250,6 +351,9 @@ export default {
           font-size: 18px;
           line-height: 28px;
           letter-spacing: -0.03em;
+          &:deep(.q-icon){
+            font-size: 22px;
+          }
         }
       }
     }
@@ -306,6 +410,17 @@ export default {
       }
 
     }
+    .more-details{
+      .btn{
+        &:deep(.q-btn__content){
+          margin: 0 !important;
+          font-weight: 600;
+          .q-icon{
+            font-size: 20px;
+          }
+        }
+      }
+    }
   }
   .img-box{
     width: 400px;
@@ -320,6 +435,7 @@ export default {
   background: #FFFFFF;
   border-radius: 16px;
   padding: 30px 80px;
+  margin-bottom: 80px;
   .info{
     font-style: normal;
     font-weight: 400;
@@ -327,6 +443,74 @@ export default {
     line-height: 28px;
     letter-spacing: -0.03em;
     color: #000000;
+  }
+  .btn-style{
+     width: 96px;
+    color: #6D708B;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 22px;
+    align-items: center;
+    text-align: center;
+    letter-spacing: -0.03em;
+  }
+}
+.sea-exams{
+  background: #FFFFFF;
+  box-shadow: -2px -4px 10px rgba(255, 255, 255, 0.6), 2px 4px 10px rgba(112, 108, 162, 0.05);
+  border-radius: 24px;
+  padding: 40px 120px;
+  margin-bottom: 80px;
+  .content{
+    margin-right: 135px;
+    width: 587px;
+    .title{
+      font-style: normal;
+      font-weight: 800;
+      font-size: 40px;
+      line-height: 62px;
+      color: #FFC107;
+      margin-bottom: 12px;
+    }
+    .sub-title{
+      font-style: normal;
+      font-weight: 600;
+      font-size: 22px;
+      line-height: 34px;
+      letter-spacing: -0.03em;
+      color: #6D708B;
+      margin-bottom: 10px;
+    }
+    .item{
+      font-style: normal;
+      font-weight: 400;
+      font-size: 16px;
+      line-height: 25px;
+      letter-spacing: -0.03em;
+      color: #6D708B;
+      margin-bottom: 6px;
+
+    }
+    .more-details{
+      .btn{
+        &:deep(.q-btn__content){
+          margin: 0 !important;
+          font-weight: 600;
+          .q-icon{
+            font-size: 20px;
+          }
+        }
+      }
+    }
+  }
+  .img-box{
+    width: 400px;
+    height: 400px;
+    .img{
+      width: 100%;
+      height: 100%;
+    }
   }
 }
 </style>
