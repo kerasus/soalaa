4b53a4c94a3f93df897daac609ee972168ff084a
mitraKhm <mitra.kh1995@gmail.com>
2022-09-21 14:45:13 +0430

    :) good luck

---
+++ src/pages/User/MyOrders/actionBtn.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-btn icon="isax:rotate-left"
+         class="reload-icon bg-white"
+         unelevated
+         @click="resetData" />
+  <q-btn unelevated
+         class="filter-btn"
+         color="primary"
+         padding="1px 23px"
+         label="اعمال"
+         @click="filterTable" />
+</template>
+
+<script>
+export default {
+  name: 'actionBtn'
+}
+</script>
+
+<style scoped lang="scss">
+.filter-btn{
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 22px;
+  letter-spacing: -0.03em;
+  color: #FFFFFF;
+}
+.reload-icon{
+  color: #6D708B;
+  margin-right: 16px;
+}
+</style>
