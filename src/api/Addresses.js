35679d0417b5996d10f75189d3957701b645173d
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-09-22 16:18:23 +0330

    user create exam apis

--- src/api/Addresses.js
+++ src/api/Addresses.js
@@ -139,7 +139,17 @@ const API_ADDRESS = {
     examBookletUpload (examId) {
       return lumenServer + '/exam-question/booklet-file/' + examId
     },
-    detachCategory: (examId, categoryId) => lumenServer + '/exam/detach/category/' + examId + '/' + categoryId
+    detachCategory: (examId, categoryId) => lumenServer + '/exam/detach/category/' + examId + '/' + categoryId ,
+    user: {
+      create: lumenServer + '/exam/user',
+      update (examId) { return lumenServer + '/exam/user' + examId },
+      draft () { return lumenServer + '/exam/user/draft' },
+      reportType: lumenServer + '/option/user?type=question_report_type',
+      report(questionId) { return lumenServer + '/question/report/store/' + questionId },
+      attach: lumenServer + '/exam-question/user/attach/v3',
+      detach(questionId) { return lumenServer + '/exam-question/user/detach/' + questionId },
+      attached(examId) { return lumenServer + '/exam-question/user/attach/show/' + examId }
+    }
   },
   question: {
     photo (type, id) {
