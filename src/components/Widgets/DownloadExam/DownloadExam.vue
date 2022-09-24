e5d5810542193adfd354aaf2ee2318c0ef747326
mitraKhm <mitra.kh1995@gmail.com>
2022-09-24 17:34:31 +0330

    update side menu style

--- src/components/Widgets/DownloadExam/DownloadExam.vue
+++ src/components/Widgets/DownloadExam/DownloadExam.vue
@@ -1,129 +1,154 @@
 <template>
   <div class="download-exam row">
     <div class="col-3 form">
-      <q-card class="exam-info q-col-gutter-y-sm row">
-        <div class="col-12">
-          <div class="header flex justify-between">
-            <div class="title">اطلاعات آزمون </div>
-            <div class="disable-all">غیرفعال کردن همه</div>
-          </div></div>
-        <div class="col-12">
-          <div class="flex justify-between items-center">
-            <q-checkbox v-model="pafConfig.hasTitle">
-              عنوان آزمون
-            </q-checkbox>
-            <div class="value">
-              gtgytr
-            </div>
-          </div></div>
-        <div class="col-12">
-          <div class="flex justify-between items-center">
-            <q-checkbox v-model="pafConfig.hasMajor">
-              رشته تحصیلی
-            </q-checkbox>
-            <div class="value">
-              gtgytr
-            </div>
-          </div></div>
-        <div class="col-12">
-          <div class="flex justify-between items-center">
-            <q-checkbox v-model="pafConfig.hasGrade">
-              پایه تحصیلی
-            </q-checkbox>
-            <div class="value">
-              gtgytr
-            </div>
-          </div></div>
-        <div class="col-12">
+      <q-card>
+        <div class="exam-info q-col-gutter-y-sm row">
+          <div class="col-12">
+            <div class="header flex justify-between">
+              <div class="title">اطلاعات آزمون </div>
+              <div class="disable-all">غیرفعال کردن همه</div>
+            </div></div>
+          <div class="col-12">
+            <div class="flex justify-between items-center">
+              <q-checkbox v-model="pafConfig.hasTitle">
+                عنوان آزمون
+              </q-checkbox>
+              <div class="value">
+                gtgytr
+              </div>
+            </div></div>
+          <div class="col-12">
+            <div class="flex justify-between items-center">
+              <q-checkbox v-model="pafConfig.hasMajor">
+                رشته تحصیلی
+              </q-checkbox>
+              <div class="value">
+                gtgytr
+              </div>
+            </div></div>
+          <div class="col-12">
+            <div class="flex justify-between items-center">
+              <q-checkbox v-model="pafConfig.hasGrade">
+                پایه تحصیلی
+              </q-checkbox>
+              <div class="value">
+                gtgytr
+              </div>
+            </div></div>
+          <div class="col-12">
+            <q-checkbox v-model="pafConfig.hasCreator">
+              منبع / طراح سوال
+            </q-checkbox></div>
           <q-checkbox v-model="pafConfig.hasCreator">
-            منبع / طراح سوال
-          </q-checkbox></div>
-        <q-checkbox v-model="pafConfig.hasCreator">
-          سال طراحی سوال
-        </q-checkbox>
-      </q-card>
-      <q-card class="spaces">
-        <div class="header flex justify-between">
-          <div class="title">فاصله گذاری </div>
-          <div class="disable-all"> حدف همه</div>
+            سال طراحی سوال
+          </q-checkbox>
         </div>
-        <div class="sub-title">حاشیه اطراف کاغذ</div>
-        <div class="l-t flex justify-between">
+        <q-separator class="separator-margin" />
+        <div class="spaces">
+          <div class="header flex justify-between">
+            <div class="title">فاصله گذاری </div>
+            <div class="disable-all"> حدف همه</div>
+          </div>
+          <div class="sub-title">حاشیه اطراف کاغذ</div>
+          <div class="l-t flex justify-between">
+            <q-input v-model="number"
+                     filled
+                     class="side-input"
+                     prefix=" cm | "
+            >
+              <template v-slot:before>
+                راست
+              </template>
+            </q-input>
+            <q-input v-model="number"
+                     filled
+                     class="side-input"
+                     prefix=" cm | "
+            >
+              <template v-slot:before>
+                چـــــپ
+              </template>
+            </q-input>
+          </div>
+          <div class="u-p flex justify-between">
+            <q-input v-model="number"
+                     filled
+                     class="side-input"
+                     prefix=" cm | "
+            >
+              <template v-slot:before>
+                بـــــــالا
+              </template>
+            </q-input>
+            <q-input v-model="number"
+                     filled
+                     class="side-input"
+                     prefix=" cm | "
+            >
+              <template v-slot:before>
+                پاییـن
+              </template>
+            </q-input>
+          </div>
+          <div class="sub-sub-title">
+            فاصله بین سوالات
+          </div>
           <q-input v-model="number"
+                   type="number"
                    filled
-                   class="side-input"
                    prefix=" cm | "
-          >
-            <template v-slot:before>
-              راست
-            </template>
-          </q-input>
-          <q-input v-model="number"
-                   filled
-                   class="side-input"
-                   prefix=" cm | "
-          >
-            <template v-slot:before>
-              چـــــپ
-            </template>
-          </q-input>
-        </div>
-        <div class="u-p flex justify-between">
-          <q-input v-model="number"
-                   filled
-                   class="side-input"
-                   prefix=" cm | "
-          >
-            <template v-slot:before>
-              بـــــــالا
-            </template>
-          </q-input>
-          <q-input v-model="number"
-                   filled
-                   class="side-input"
-                   prefix=" cm | "
-          >
-            <template v-slot:before>
-              پاییـن
-            </template>
-          </q-input>
-        </div>
-        <div class="sub-sub-title">
-          فاصله بین سوالات
-        </div>
-        <q-input v-model="number"
-                 type="number"
-                 filled
-                 prefix=" cm | "
-        />
+          />
 
-      </q-card>
-      <q-card class="question-info">
-        <div class="header flex justify-between">
-          <div class="title">شماره گذاری </div>
-          <div class="disable-all"> حدف همه</div>
         </div>
-        <div class="sub-title">
-          شماره شروع سوالات
+        <q-separator class="separator-margin" />
+        <div class="question-info">
+          <div class="header flex justify-between">
+            <div class="title">شماره گذاری </div>
+            <div class="disable-all"> حدف همه</div>
+          </div>
+          <div class="sub-title">
+            شماره شروع سوالات
+          </div>
+          <q-input v-model="number"
+                   type="number"
+                   filled
+                   prefix=" cm | "
+          />
+          <div class="sub-title">
+            صفحه بندی سوالات
+          </div>
+          <q-input v-model="number"
+                   type="number"
+                   filled
+                   prefix=" cm | "
+          />
         </div>
-        <q-input v-model="number"
-                 type="number"
-                 filled
-                 prefix=" cm | "
-        />
-        <div class="sub-title">
-          صفحه بندی سوالات
-        </div>
-        <q-input v-model="number"
-                 type="number"
-                 filled
-                 prefix=" cm | "
-        />
+
+        <q-btn
+          unelevated
+          color="primary"
+          class="submit-btn full-width">
+          <span class="btn-label">
+            اعمال
+          </span>
+        </q-btn>
       </q-card>
     </div>
     <div class="col-9 pdf ">
       <q-card>
-        fg
+        <q-tabs
+          v-model="tab"
+          class="test-tabs"
+          active-color="secondary"
+          align="left"
+        >
+          <q-tab name="test"
+                 label="سوالات" />
+          <q-tab name="myTest"
+                 label="پاسخنامه تشریحی" />
+          <q-tab name="mymyTest"
+                 label="پاسخنامه کلیدی" />
+        </q-tabs>
       </q-card>
     </div>
 
@@ -134,6 +159,7 @@
 export default {
   name: 'DownloadExam',
   data: () => ({
+    tab: 'test',
     pafConfig: {
       hasTitle: false,
       hasMajor: true,
@@ -174,6 +200,21 @@ export default {
     }
   }
   .form{
+    .separator-margin{
+      margin: 20px 0;
+    }
+.submit-btn{
+  margin-top: 38.5px;
+  .btn-label{
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 25px;
+    align-items: center;
+    letter-spacing: -0.03em;
+    color: #FFFFFF;
+  }
+}
+
     padding-right: 15px;
     .value{
       width: 155px;
@@ -246,6 +287,51 @@ export default {
   }
   .pdf{
     padding-left: 15px;
+
+    .q-tab-panels {
+      background: transparent;
+      border-top: 2px solid #E4E8EF;
+
+      .q-tab-panel {
+        padding: 2px 0;
+      }
+    }
+
+    .q-tab__content{
+      padding: 0;
+      margin: 0 17px
+    }
+
+    .test-tabs {
+      color: #8A8CA6;
+
+      &:deep(.q-tab__indicator) {
+        width: 100%;
+        height: 2px;
+        padding-right: 30px;
+        border-radius: 6px;
+      }
+
+      &:deep(.q-tab) {
+        color: var(--3a-TextSecondary);
+        padding: 0;
+      }
+
+      &:deep(.q-tab__content) {
+        padding: 0 24px;
+      }
+
+      &:deep(.q-tab__label) {
+        font-style: normal;
+        font-weight: 600;
+        font-size: 16px;
+        line-height: 25px;
+        text-align: center;
+        letter-spacing: -0.03em;
+        color: #FFA117;
+
+      }
+    }
   }
 }
 </style>
