f3aa6fb8df2bfabac433f3949eae65770ac5a264
JasGH <yasaman.gonehi@gmail.com>
2022-09-26 18:54:21 +0330

    User.Exam.List

--- src/components/Menu/SideMenu/SendAnswers.vue
+++ src/components/Menu/SideMenu/SendAnswers.vue
@@ -147,6 +147,10 @@ export default {
       const that = this
       this.sendUserQuestionsDataToServerAndFinishExam(this.quiz.user_exam_id, false)
         .then(response => {
+          this.$q.notify({
+            message: 'ثبت با موفقیت انجام شد',
+            type: 'positive'
+          })
           that.$router.push({ name: 'User.Exam.List' })
           that.confirmationBubbleSheet = true
         })
