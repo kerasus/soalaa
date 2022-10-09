d415cf726df057a5c96528bbe6fe00cf240065c1
kerasus <aliesmaeeli20@gmail.com>
2022-10-09 19:40:36 +0330

    #983 title-box bug fix in 599px

--- src/components/Widgets/Footer/Footer.vue
+++ src/components/Widgets/Footer/Footer.vue
@@ -307,6 +307,7 @@ export default {
         justify-content: center;
         .title-box {
           justify-self: center;
+          width: 100%;
         }
         .access,.profile {
           padding-bottom: 30px;
