3e4852778de9347c3ef1d52ff7e111866f3f2aaf
mitraKhm <mitra.kh1995@gmail.com>
2022-10-11 14:09:57 +0330

    fix calendarIcon

--- src/pages/Admin/exam/Create/CreateExamPage.vue
+++ src/pages/Admin/exam/Create/CreateExamPage.vue
@@ -97,7 +97,7 @@ export default {
           col: 'col-12 col-md-3 col-sm-6',
           value: [
             { type: 'separator', label: 'زمان شروع آزمون', size: '0', separatorType: 'none', col: 'col-12' },
-            { type: 'dateTime', name: 'start_at', responseKey: 'data.start_at', label: '', col: 'col-12', calendarIcon: 'isax:calendar-1', placeholder: 'زمان شروع آزمون' }
+            { type: 'dateTime', name: 'start_at', responseKey: 'data.start_at', label: '', col: 'col-12', calendarIcon: ' ', placeholder: 'زمان شروع آزمون' }
           ]
         },
         {
