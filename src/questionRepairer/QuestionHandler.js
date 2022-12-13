ca48f1d79081f2070497390a6c73d2d2e5cf5c64
JasGH <yasaman.gonehi@gmail.com>
2022-12-13 13:22:45 +0330

    update

--- src/questionRepairer/QuestionHandler.js
+++ src/questionRepairer/QuestionHandler.js
@@ -1,5 +1,5 @@
 import QuestionModifier from 'src/questionRepairer/QuestionModifier'
-import API_ADDRESS from 'src/api/Addresses'
+// import API_ADDRESS from 'src/api/Addresses'
 import { Notify } from 'quasar'
 import { saveAs } from 'file-saver'
 class QuestionHandler {
@@ -14,151 +14,147 @@ class QuestionHandler {
     this.attachExamList = [
       {
         examName: 'EmptyFormulaElements',
-        examId: '6387342be1685e81610c0008',
+        examId: '639713ed4121b90fa30ca76e',
         questions: [],
         isPending: false
       },
       {
         examName: 'ParagraphWithTwoDollarSigns',
-        examId: '638738d8e1685e81610c001f',
+        examId: '639713cdeb6a9f69ae079418',
         questions: [],
         isPending: false
       },
       {
         examName: 'DataKatexElInTwoDollarSigns',
-        examId: '6387390be1685e81610c0025',
+        examId: '639713bc4121b90fa30ca75c',
         questions: [],
         isPending: false
       },
       {
         examName: 'OddNumberOfDollarSigns',
-        examId: '6387391ce1685e81610c002a',
+        examId: '6397139c4121b90fa30ca753',
         questions: [],
         isPending: false
       },
       {
         examName: 'UnclosedFormulaBracket',
-        examId: '6387394ae1685e81610c0033',
+        examId: '6397137d4121b90fa30ca747',
         questions: [],
         isPending: false
       },
       {
         examName: 'NotCoddedInequalitySigns',
-        examId: '6387398de1685e81610c0038',
+        examId: '639713644121b90fa30ca737',
         questions: [],
         isPending: false
       },
       {
         examName: 'ElementWithPossibleLossOfPersianWordsCaretSymbol',
-        examId: '638739b5e1685e81610c003d',
+        examId: '6397134d4121b90fa30ca728',
         questions: [],
         isPending: false
       },
       {
         examName: 'ElementWithHtmlStyleTag',
-        examId: '63873a6fe1685e81610c0047',
+        examId: '639713384121b90fa30ca717',
         questions: [],
         isPending: false
       },
       {
         examName: 'ElementWithTheOverlineTag',
-        examId: '63873aa7e1685e81610c004d',
+        examId: '6397131d4121b90fa30ca705',
         questions: [],
         isPending: false
       },
       {
         examName: 'ElementWithUnderBraces',
-        examId: '63873acee1685e81610c0053',
+        examId: '639713084121b90fa30ca6f2',
         questions: [],
         isPending: false
       },
       {
         examName: 'ElementWithPossibleLossOfPersianWordsBackslashSymbol',
-        examId: '63873b00e1685e81610c0058',
+        examId: '639712f04121b90fa30ca6de',
         questions: [],
         isPending: false
       },
       {
         examName: 'PossibilityOfTextLossForInequalitySigns',
-        examId: '63873b27e1685e81610c005d',
+        examId: '639712c64121b90fa30ca6ca',
         questions: [],
         isPending: false
       },
       {
         examName: 'ElementsWithPrimeAndPower',
-        examId: '63873b4ae1685e81610c0062',
+        examId: '639712a84121b90fa30ca6b6',
         questions: [],
         isPending: false
       },
       {
         examName: 'ElementsWithWrongCombination',
-        examId: '63873b74e1685e81610c0068',
+        examId: '6397125d4121b90fa30ca69f',
         questions: [],
         isPending: false
       },
       {
         examName: 'ElementsWithWrongSinus',
-        examId: '63873bb2e1685e81610c006d',
+        examId: '6397123b4121b90fa30ca689',
         questions: [],
         isPending: false
       },
       {
         examName: 'ElementWithCurlyBrackets',
-        examId: '63873c31e1685e81610c0073',
+        examId: '639712264121b90fa30ca672',
         questions: [],
         isPending: false
       },
       {
         examName: 'ElementsWithWidehatTag',
-        examId: '63873c7ce1685e81610c007d',
+        examId: '639712074121b90fa30ca65a',
         questions: [],
         isPending: false
       },
       {
         examName: 'ElementsWithWrongCosine',
-        examId: '638af2b9e1685e81610c0100',
+        examId: '639711ed4121b90fa30ca644',
         questions: [],
         isPending: false
       },
       {
         examName: 'allFailedUpdateQuestions',
-        examId: '638b6604426ad5e8ad008bdb',
+        examId: '639711cf4121b90fa30ca629',
         questions: [],
         isPending: false
       },
       {
         examName: 'ElementWithWrongTag',
-        examId: '639490421e536a80ff052bbd',
+        examId: '639710a64121b90fa30ca608',
         questions: [],
         isPending: false
       }
     ]
+    this.countOfUpdateRequest = 0
+    this.arrayOfUpdateRequest = []
   }
 
-  // {
-  //   "exam_id": "{{exam_id}}",
-  //   "questions": [
-  //     {
-  //       "question_id": "5ffdd6ad077f6b34e529bc28",
-  //       "sub_category_id": "60b7875428f350277f04c5e8",
-  //       "order": "{{random_int}}"
-  //     },
-  // }
   attachQuestionToExamRequest (attachExamObj) {
     if (!(attachExamObj.questions.length > 0)) {
       return new Promise((resolve, reject) => {
         resolve()
       })
     }
-    return this.$axios.post(API_ADDRESS.exam.bulkAttachQuestions(), {
-      exam_id: attachExamObj.examId,
-      questions: attachExamObj.questions.map((questionModifier, index) => {
-        return {
-          order: index + 1,
-          sub_category_id: '60b7875428f350277f04c5e3',
-          question_id: questionModifier.question.id
-        }
-      })
+    // return this.$axios.post(API_ADDRESS.exam.bulkAttachQuestions(), {
+    //   exam_id: attachExamObj.examId,
+    //   questions: attachExamObj.questions.map((questionModifier, index) => {
+    //     return {
+    //       order: index + 1,
+    //       sub_category_id: '60b7875428f350277f04c5e3',
+    //       question_id: questionModifier.question.id
+    //     }
+    //   })
+    // })
+    return new Promise((resolve, reject) => {
+      resolve()
     })
   }
 
@@ -245,165 +241,6 @@ class QuestionHandler {
 
   initHandler () {
     return new Promise((resolve, reject) => {
-      // for (let i = 0; i < 1; i++) {
-      //   const q = {
-      //     data: {
-      //       id: '6288a0038d2697d26d0607bb',
-      //       statement: '<p dir="auto" style="text-align: right">در مثلث ABC، نیمساز داخلی زاویۀ A ضلع<span data-katex="true">$\\overline {BC}$</span>را در نقطۀ D قطع می‌کند. کدام نامساوی همواره صحیح است؟</p>',
-      //       choices: [
-      //         {
-      //           id: 1,
-      //           title: '<p dir="auto" style="text-align: right"><span data-katex="true">$BA&amp;gt;BD$</span></p>',
-      //           answer: true,
-      //           order: 0
-      //         },
-      //         {
-      //           id: 2,
-      //           title: '<p dir="auto" style="text-align: right"><span data-katex="true">$DA&amp;gt;DB$</span></p>',
-      //           answer: false,
-      //           order: 1
-      //         },
-      //         {
-      //           id: 3,
-      //           title: '<p dir="auto" style="text-align: right"><span data-katex="true">$AB&amp;gt;AD$</span></p>',
-      //           answer: false,
-      //           order: 2
-      //         },
-      //         {
-      //           id: 4,
-      //           title: '<p dir="auto" style="text-align: right"><span data-katex="true">$DB&amp;gt;DA$</span></p>',
-      //           answer: false,
-      //           order: 3
-      //         }
-      //       ],
-      //       exams: [
-      //         {
-      //           sub_category: {
-      //             id: '60b7875428f350277f04c5ec',
-      //             title: 'هندسه',
-      //             category_id: '60b7858d743940688b23c7f4'
-      //           },
-      //           category_title: 'دفترچه سؤالات اختصاصی',
-      //           exam_id: '627f2c8043bf55650e044228',
-      //           order: 69,
-      //           in_subcategory_order: 69,
-      //           exam: {
-      //             id: '627f2c8043bf55650e044228',
-      //             title: 'پاور تست هندسۀ دهم',
-      //             holding_status: null,
-      //             start_at: '2022-04-21 08:42:00',
-      //             finish_at: '2022-07-16 08:42:00',
-      //             photo: 'https://cdn.alaatv.com/upload/images/slideShow/home-slide-yalda-festival_20201219075413.jpg?w=1843&h=719',
-      //             delay_time: 100,
-      //             n_questions: 137,
-      //             enable: false,
-      //             updated_at: '2022-05-21 14:01:40',
-      //             created_at: '2022-05-14 08:43:52',
-      //             generate_questions_automatically: false,
-      //             confirm: false,
-      //             report_config: [],
-      //             holding_config: {
-      //               randomize_questions: false
-      //             },
-      //             alaa_product_link: null,
-      //             type: {
-      //               id: '6225f4828044517f52500c02',
-      //               type: 'exam_type',
-      //               value: 'konkur',
-      //               updated_at: '2022-03-07 15:33:14',
-      //               created_at: '2022-03-07 15:33:14'
-      //             },
-      //             user_exam_id: null,
-      //             is_free: false,
-      //             is_register_open: false,
-      //             is_open: false
-      //           }
-      //         }
-      //       ],
-      //       level: '3',
-      //       photos: [
-      //         'https://nodes.alaatv.com/aaa/questionPhotos/Screenshot-2022-11-12-032524-4437527.png'
-      //       ],
-      //       confirmers: [],
-      //       confirmed: false,
-      // eslint-disable-next-line no-irregular-whitespace
-      //       descriptive_answer: '<p dir="auto"><span data-katex="true">$\\overline {AD}$</span>نیم ساز <span data-katex="true">$\\mathop A\\limits^ \\wedge\\]است.\n\\[\\begin{array}{l}\nm({\\mathop A\\limits^ \\wedge  _1}) = m({\\mathop A\\limits^ \\wedge  _2})\\\\\nm({\\mathop D\\limits^ \\wedge  _2}) = m({\\mathop A\\limits^ \\wedge  _1}) + m(\\mathop B\\limits^ \\wedge  )\\\\\nm({\\mathop D\\limits^ \\wedge  _1}) = m({\\mathop A\\limits^ \\wedge  _2}) + m(\\mathop C\\limits^ \\wedge  )\n\\end{array}$</span></p><p dir="auto">در<span data-katex="true">$m({\\mathop A\\limits^ \\wedge  _1}) &amp;lt; m(A\\mathop D\\limits^ \\wedge  B),B\\mathop A\\limits^\\Delta  D$</span> ، زیرا <span data-katex="true">$m(A\\mathop D\\limits^ \\wedge  B) = m({\\mathop A\\limits^ \\wedge  _2}) + C$</span>. پس: <span data-katex="true">$BD &amp;lt; AB$</span></p><p dir="auto"><img src="https://nodes.alaatv.com/aaa/questionPhotos/Screenshot-2022-11-12-032524-4437527.png" width="324" height="204" data-vertical="-1" style="vertical-align: 1px"></p><p dir="auto"></p>',
-      //       statement_photo: [
-      //         'https://nodes.alaatv.com/aaa/questionStatements/6288a0038d2697d26d0607bb/163-7803056.png'
-      //       ],
-      //       answer_photos: [
-      //         'https://nodes.alaatv.com/aaa/questionStatements/6288a0038d2697d26d0607bb/163-5203436.png'
-      //       ],
-      //       status: {
-      //         id: '60c7102418e65826bc7da378',
-      //         title: 'typed',
-      //         display_title: 'تایپ شده',
-      //         updated_at: '2021-06-14 12:45:32',
-      //         created_at: '2021-06-14 12:45:32'
-      //       },
-      //       type: {
-      //         value: 'konkur'
-      //       },
-      //       tags: [
-      //         {
-      //           id: '62fb2c684dcdcce15d084f69',
-      //           title: 'درس دوم: استدلال',
-      //           parent: {
-      //             id: '62fb2b614dcdcce15d084f5d',
-      //             title: 'فصل اول: ترسیم های هندسی و استدلال'
-      //           },
-      //           ancestors: [
-      //             {
-      //               id: '6281fb9e2f1bafe99f050334',
-      //               title: 'درخت دانش'
-      //             },
-      //             {
-      //               id: '62947d2624d62d858d09a4d2',
-      //               title: 'پایه دهم'
-      //             },
-      //             {
-      //               id: '629dba2f8c4d7e26720cece2',
-      //               title: 'هندسه ۱'
-      //             },
-      //             {
-      //               id: '62fb2b614dcdcce15d084f5d',
-      //               title: 'فصل اول: ترسیم های هندسی و استدلال'
-      //             }
-      //           ],
-      //           order: '2',
-      //           type: null,
-      //           children: [],
-      //           updated_at: '2022-08-16 10:04:32',
-      //           created_at: '2022-08-16 10:04:32'
-      //         }
-      //       ],
-      //       designers: [],
-      //       reference: [
-      //         {
-      //           id: '62f384484123bae3fc0b5423',
-      //           type: 'reference_type',
-      //           value: 'برگزیده سوالات کتب کمک آموزشی',
-      //           updated_at: '2022-09-01 11:40:09',
-      //           created_at: '2022-08-10 14:41:20'
-      //         }
-      //       ],
-      //       years: [],
-      //       majors: [
-      //         {
-      //           id: '62f38507eaeeaf1f0c0d81b4',
-      //           type: 'major_type',
-      //           value: 'ریاضی',
-      //           updated_at: '2022-08-10 14:44:31',
-      //           created_at: '2022-08-10 14:44:31'
-      //         }
-      //       ],
-      //       targets: [],
-      //       updated_at: '2022-11-12 20:07:41',
-      //       created_at: '2022-05-21 12:47:07'
-      //     }
-      //   }
-      //   this.initQuestionModifier(q.data)
-      // }
       this.arrayOfQuestions.forEach((question) => {
         this.initQuestionModifier(question)
       })
