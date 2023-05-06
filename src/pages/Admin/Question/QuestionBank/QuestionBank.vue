7c9e4e158c424ceb0cd4485a07e9f921bc8e74e0
ali-khoshnevisan <ali.kh.3437@gmail.com>
2023-05-06 10:28:42 +0330

    question group

--- src/pages/Admin/Question/QuestionBank/QuestionBank.vue
+++ src/pages/Admin/Question/QuestionBank/QuestionBank.vue
@@ -72,13 +72,14 @@
                          :question="loadingQuestion"
           />
           <template v-else>
-            <question-item v-for="question in questions.list"
-                           :key="question.id"
-                           :question="question"
-                           :listOptions="questionsOptions"
-                           pageStrategy="question-bank"
-                           @deleteFromDb="deleteQuestionFromDataBase"
-                           @checkSelect="onClickedCheckQuestionBtn"
+            <question-item
+              v-for="question in questions.list"
+              :key="question.id"
+              :question="question"
+              :listOptions="questionsOptions"
+              pageStrategy="question-bank"
+              @deleteFromDb="deleteQuestionFromDataBase"
+              @checkSelect="onClickedCheckQuestionBtn"
             />
           </template>
         </div>
