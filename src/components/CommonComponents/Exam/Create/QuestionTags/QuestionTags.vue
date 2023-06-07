fb6539968f1aac825954e0d3452d0e5c1ec0c09a
ali-khoshnevisan <ali.kh.3437@gmail.com>
2023-06-07 16:17:04 +0330

    tags

---
+++ src/components/CommonComponents/Exam/Create/QuestionTags/QuestionTags.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    v-for="(item, index) in question.tags.list"
+    :key="index"
+    class="question-tag ellipsis"
+  >
+    <span v-for="(ancestor,ancestorIndex) in item.ancestors"
+          :key="ancestorIndex"
+          class="ancestors">
+      <span v-if="ancestorIndex !== 0"
+            class="tag-title">{{ ancestor.title }}</span>
+      <span v-if="ancestorIndex !== 0"
+            class="tag-circle" />
+    </span>
+    <span>
+      <span class="title-circle" />
+      <span class="tag-title">{{ item.title }}</span>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuestionTags'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/CommonComponents/Exam/Create/QuestionTags/components/tagsComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    v-for="(item, index) in tags"
+    :key="index"
+    class="question-tag ellipsis"
+  >
+    <span v-for="(ancestor,ancestorIndex) in item.ancestors"
+          :key="ancestorIndex"
+          class="ancestors">
+      <span v-if="ancestorIndex !== 0"
+            class="tag-title">{{ ancestor.title }}</span>
+      <span v-if="ancestorIndex !== 0"
+            class="tag-circle" />
+    </span>
+    <span>
+      <span class="title-circle" />
+      <span class="tag-title">{{ item.title }}</span>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tagsComponent',
+  props: {
+    tags: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
