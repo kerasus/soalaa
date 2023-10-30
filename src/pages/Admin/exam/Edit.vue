d1963e01108779d512fcb1e0e20e4a27f532be91
Ayhanemoon <ayhan.samimi@example.com>
2023-10-30 16:44:51 +0330

    exam answers edit fixed

--- src/pages/Admin/exam/Edit.vue
+++ src/pages/Admin/exam/Edit.vue
@@ -19,7 +19,7 @@
       <q-separator />
       <q-card-section class="flex">
         <div class="row bg-grey-3 add-category-box">
-          <q-select v-model="category.title"
+          <q-select v-model="category.id"
                     class="q-pa-md col-md-4"
                     :value="category"
                     label="دفترچه"
@@ -241,8 +241,9 @@ export default {
       if (this.totalCategory) {
         return
       }
-
+      console.log('out', this.category)
       if (this.category.title.id) {
+        console.log('inIf: ', this.category)
         this.category.id = this.category.title.id
         this.category.title = this.category.title.title
       }
