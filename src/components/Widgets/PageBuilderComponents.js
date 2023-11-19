5679630f8bae0d6a958bb62e1f409d620fad3384
ali-khoshnevisan <ali.kh.3437@gmail.com>
2023-11-19 12:18:13 +0330

    refactor: merge two components DownloadExam

--- src/components/Widgets/PageBuilderComponents.js
+++ src/components/Widgets/PageBuilderComponents.js
@@ -8,7 +8,6 @@ export default {
   ComparisonTable: defineAsyncComponent(() => import('src/components/Widgets/ComparisonTable/ComparisonTable.vue')),
   ContentItem: defineAsyncComponent(() => import('src/components/Widgets/ContentItem/ContentItem.vue')),
   Dashboard: defineAsyncComponent(() => import('src/components/Widgets/Dashboard/Dashboard.vue')),
-  DownloadExam: defineAsyncComponent(() => import('src/components/Widgets/DownloadExam/DownloadExam.vue')),
   DownloadSoalaaExam: defineAsyncComponent(() => import('src/components/Widgets/DownloadSoalaaExam/DownloadSoalaaExam.vue')),
   ExpansionPanel: defineAsyncComponent(() => import('src/components/Widgets/ExpansionPanel/ExpansionPanel.vue')),
   FeatureBox: defineAsyncComponent(() => import('src/components/Widgets/FeatureBox/FeatureBox.vue')),
